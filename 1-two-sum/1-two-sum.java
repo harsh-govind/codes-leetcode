81e22c3833f5560d08bc1aaf5065250033f9c81f
Harsh Govind <71700319+harsh-govind@users.noreply.github.com>
2022-06-29 22:20:29 +0530

    Time: 50 ms (42.19%), Space: 42.6 MB (87.71%) - LeetHub

---
+++ 1-two-sum/1-two-sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] arr, int target) {
+        int ans[]= new int[2], n=arr.length;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[i]+arr[j]==target)
+            {
+                ans[0]=i;
+                ans[1]=j;
+                return ans;
+            }
+        }
+    }
+        return ans;
+    }
+}
