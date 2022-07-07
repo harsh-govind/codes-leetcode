1faf4c13806076fb3e1417bb9823364a3a9585a2
Harsh Govind <71700319+harsh-govind@users.noreply.github.com>
2022-07-07 11:12:03 +0530

    Time: 3 ms (64.06%), Space: 8.6 MB (85.58%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -2,39 +2,23 @@ class Solution
 {
     public:
         vector<int> plusOne(vector<int> &v)
-{
-    int n = v.size();
-    // if (v[n - 1] < 9)
-    // {
-    //     v[n - 1]++;
-    //     return v;
-    // }
-    // else
-    // if (v[n - 1] == 9 and n == 1)
-    // {
-    //     v.clear();
-    //     v.push_back(1);
-    //     v.push_back(0);
-    //     return v;
-    // }
-    // else
-    // {
-    int i = n - 1;
-    while (i > -1)
-    {
-        if (v[i] < 9)
         {
-            v[i]++;
+            int n = v.size();
+            int i = n - 1;
+            while (i > -1)
+            {
+                if (v[i] < 9)
+                {
+                    v[i]++;
+                    return v;
+                }
+                else
+                {
+                    v[i] = 0;
+                    i--;
+                }
+            }
+            v.insert(v.begin(), 1);
             return v;
         }
-        else
-        {
-            v[i] = 0;
-            i--;
-        }
-    }
-    // }
-    v.insert(v.begin(), 1);
-    return v;
-}
 };
