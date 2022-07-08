db6d614407b740b2103981f5587cface6c130788
Harsh Govind <71700319+harsh-govind@users.noreply.github.com>
2022-07-08 12:22:45 +0530

    Time: 20 ms (85.92%), Space: 18 MB (19.02%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,9 +1,11 @@
-class Solution {
-public:
-    int missingNumber(vector<int>& v) {
-     int n=v.size();
-        n=(n*(n+1))/2;
-        int sum = accumulate(v.begin(), v.end(), 0);
-        return n-sum;
-    }
+class Solution
+{
+    public:
+        int missingNumber(vector<int> &v)
+        {
+            int n = v.size();
+            n = (n *(n + 1)) / 2;
+            int sum = accumulate(v.begin(), v.end(), 0);
+            return n - sum;
+        }
 };
