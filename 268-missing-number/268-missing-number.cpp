e7eb856478654efb787702795b600e7340b51565
Harsh Govind <71700319+harsh-govind@users.noreply.github.com>
2022-07-08 12:22:40 +0530

    Time: 37 ms (27.45%), Space: 17.8 MB (93.46%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,20 +1,9 @@
 class Solution {
 public:
     int missingNumber(vector<int>& v) {
-    long long actual2, given2, actual, given, n = v.size();
-    actual2 = given2 = actual = given = 0;
-    actual = (n * (n + 1)) / 2;
-    actual2 = (n * (n + 1) * (2 * n + 1)) / 6;
-    for (auto &val : v)
-    {
-        given += (long long)val;
-        given2 += ((long long)val * (long long)val);
-    }
-    long long a = actual - given;
-        if(a==0)
-            return 0;
-    long long k = (actual2 - given2) / a;
-    long long m = (k + a) / 2;
-   return m;
+     int n=v.size();
+        n=(n*(n+1))/2;
+        int sum = accumulate(v.begin(), v.end(), 0);
+        return n-sum;
     }
 };
