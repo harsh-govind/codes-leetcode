164ca8a30d36cdc6be01d2465d34fcbbf4e8fc14
Harsh Govind <71700319+harsh-govind@users.noreply.github.com>
2022-07-10 19:03:09 +0530

    Time: 35 ms (5.65%), Space: 16.9 MB (7.49%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.cpp
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
         string ans="";
-        map<int, char> m;
+        unordered_map<int, char> m;
         for(int i=0;i<s.size();i++)
         {
             m[indices[i]]=s[i];
         }
-        for(auto &val:m)
+        for(int i=0;i<s.size();i++)
         {
-            ans+=val.second;
+            ans+=m[i];
         }
         return ans;
     }
