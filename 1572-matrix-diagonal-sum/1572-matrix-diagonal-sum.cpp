7e21478dfa820e13a7aabc94a54172dd479dbaf4
Harsh Govind <71700319+harsh-govind@users.noreply.github.com>
2022-07-12 22:11:22 +0530

    Time: 38 ms (5.06%), Space: 11.4 MB (11.15%) - LeetHub

---
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+    public:
+        int diagonalSum(vector<vector < int>> &mat)
+        {
+            int n = mat.size(), ans = 0;
+            for (int i = 0; i < n; i++)
+            {
+                ans += mat[i][i];
+                if (n - 1 - i != i)
+                    ans += mat[i][n - 1 - i];
+            }
+            return ans;
+        }
+};
