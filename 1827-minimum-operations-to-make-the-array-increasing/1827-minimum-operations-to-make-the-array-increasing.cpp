6eae25d8463a9bb3be3fd03f40bc4b677d3b6583
Harsh Govind <71700319+harsh-govind@users.noreply.github.com>
2022-07-13 21:16:35 +0530

    Time: 13 ms (92.49%), Space: 15.8 MB (77.95%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.cpp
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.cpp
@@ -1,17 +1,18 @@
-class Solution {
-public:
-    int minOperations(vector<int>& nums) {
-        int ans=0;
-        for(int i=1;i<nums.size();i++)
+class Solution
+{
+    public:
+        int minOperations(vector<int> &nums)
         {
-            if(nums[i]<=nums[i-1])
+            int ans = 0;
+            for (int i = 1; i < nums.size(); i++)
             {
-                ans+=nums[i-1]-nums[i];
-                ans++;
-                nums[i]+=nums[i-1]-nums[i]+1;
+                if (nums[i] <= nums[i - 1])
+                {
+                    ans += nums[i - 1] - nums[i];
+                    ans++;
+                    nums[i] += nums[i - 1] - nums[i] + 1;
+                }
             }
-            cout<<nums[i]<<" ";
+            return ans;
         }
-        return ans;
-    }
 };
