5f9ee9c3a4650389c5fedf52a4b04c43d352fe47
Harsh Govind <71700319+harsh-govind@users.noreply.github.com>
2022-07-25 20:44:56 +0530

    Time: 24 ms (44.98%), Space: 13.4 MB (10.69%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -3,26 +3,26 @@ class Solution
     public:
         void setZeroes(vector<vector <int>> &vec)
         {
-            int m=vec.size(), n=vec[0].size();
+            int m = vec.size(), n = vec[0].size();
             unordered_set<int> r, c;
-            for(int i=0;i<m;i++)
+            for (int i = 0; i < m; i++)
             {
-                for(int j=0;j<n;j++)
+                for (int j = 0; j < n; j++)
                 {
-                    if(vec[i][j]==0)
+                    if (vec[i][j] == 0)
                     {
                         r.insert(i);
                         c.insert(j);
                     }
                 }
             }
-            for(int i=0;i<m;i++)
+            for (int i = 0; i < m; i++)
             {
-                for(int j=0;j<n;j++)
+                for (int j = 0; j < n; j++)
                 {
-                    if(r.find(i)!=r.end() or c.find(j)!=c.end())
+                    if (r.find(i) != r.end() or c.find(j) != c.end())
                     {
-                        vec[i][j]=0;
+                        vec[i][j] = 0;
                     }
                 }
             }
