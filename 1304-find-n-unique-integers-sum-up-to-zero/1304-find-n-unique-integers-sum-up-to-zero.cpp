43f50f765bed5f5cac7560a7a3f7160c59256ed2
Harsh Govind <71700319+harsh-govind@users.noreply.github.com>
2022-07-25 14:24:08 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (9.24%) - LeetHub

---
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+    public:
+        vector<int> sumZero(int n)
+        {
+            vector<int> ans;
+            int i = 0;
+            if (n % 2 != 0)
+            {
+                ans.push_back(0);
+                i++;
+                while (i <= n / 2)
+                {
+                    ans.push_back(i);
+                    ans.push_back(-i);
+                    i++;
+                }
+                return ans;
+            }
+            i=1;
+            while (i <= n / 2)
+            {
+                ans.push_back(i);
+                ans.push_back(-i);
+                i++;
+            }
+            return ans;
+        }
+};
