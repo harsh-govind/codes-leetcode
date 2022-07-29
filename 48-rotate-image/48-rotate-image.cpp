9e22b6e59511f439ff0061ce272dbd5e9340a183
Harsh Govind <harsh5092001@gmail.com>
2022-07-29 13:25:37 +0530

    Time: 0 ms (100.00%), Space: 7 MB (92.66%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,31 +1,24 @@
-class Solution {
-public:
-    void rotate(vector<vector<int>>& matrix) {
-        int r=matrix.size(), c=matrix[0].size();
-        
-        for(int i=0;i<r;i++)
+class Solution
+{
+    public:
+        void rotate(vector<vector < int>> &matrix)
         {
-            for(int j=0;j<=i;j++)
+            int r = matrix.size(), c = matrix[0].size();
+
+            for (int i = 0; i < r; i++)
             {
-                swap(matrix[i][j], matrix[j][i]);
+                for (int j = 0; j <= i; j++)
+                {
+                    swap(matrix[i][j], matrix[j][i]);
+                }
+            }
+
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < (c / 2); j++)
+                {
+                    swap(matrix[i][j], matrix[i][c - j - 1]);
+                }
             }
         }
-        
-        for(int i=0;i<r;i++)
-        {
-            for(int j=0;j<(c/2);j++)
-            {
-                swap(matrix[i][j], matrix[i][c-j-1]);
-            }
-        }
-        
-        for(int i=0;i<r;i++)
-        {
-            for(int j=0;j<c;j++)
-            {
-                cout<<matrix[i][j]<<" ";
-            }
-            cout<<endl;
-        }
-    }
 };
