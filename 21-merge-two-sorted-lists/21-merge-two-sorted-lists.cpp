35c7d9f0d145bf36c407820f2cf56b94cd210f4e
Harsh Govind <harsh5092001@gmail.com>
2022-08-01 01:21:55 +0530

    Time: 8 ms (80.88%), Space: 14.8 MB (81.40%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,81 +11,81 @@
 class Solution
 {
     public:
-    ListNode* mergeTwoLists(ListNode *a, ListNode *b)
-    {
-        if(!a)
+        ListNode* mergeTwoLists(ListNode *a, ListNode *b)
         {
-            return b;
-        }
-        if(!b)
-        {
-            return a;
-        }
-        ListNode *last=nullptr, *ans=nullptr;
-        
-        if(a and b and a->val < b->val)
-        {
-            last = a;
-            ans = last;
-            a = a->next;
-            last->next = nullptr;
-        }
-        else if(a and b and a->val > b->val)
-        {
-            last = b;
-            ans = last;
-            b = b->next;
-            last->next = nullptr;
-        }
-        else if(a and b)
-        {
-            last = a;
-            ans = last;
-            a = a->next;
-            last->next = b;
-            last = last->next;
-            b=b->next;
-            last->next = nullptr;
-        }
-        
-        ListNode *p=a, *q=b;
-        
-        while(p and q)
-        {
-            if(p->val < q->val)
+            if (!a)
             {
-                last->next=p;
-                p=p->next;
-                last=last->next;
-                last->next=nullptr;
+                return b;
             }
-            else if(p->val > q->val)
+            if (!b)
             {
-                last->next=q;
-                q=q->next;
-                last=last->next;
+                return a;
+            }
+            ListNode *last = nullptr, *ans = nullptr;
+
+            if (a and b and a->val < b->val)
+            {
+                last = a;
+                ans = last;
+                a = a->next;
                 last->next = nullptr;
             }
-            else
+            else if (a and b and a->val > b->val)
             {
-                ListNode *t1 = p, *t2 = q;
-                p = p->next;
-                q = q->next;
-                last->next = t1;
-                t1->next = t2;
-                last = t2;
+                last = b;
+                ans = last;
+                b = b->next;
                 last->next = nullptr;
             }
+            else if (a and b)
+            {
+                last = a;
+                ans = last;
+                a = a->next;
+                last->next = b;
+                last = last->next;
+                b = b->next;
+                last->next = nullptr;
+            }
+
+            ListNode *p = a, *q = b;
+
+            while (p and q)
+            {
+                if (p->val < q->val)
+                {
+                    last->next = p;
+                    p = p->next;
+                    last = last->next;
+                    last->next = nullptr;
+                }
+                else if (p->val > q->val)
+                {
+                    last->next = q;
+                    q = q->next;
+                    last = last->next;
+                    last->next = nullptr;
+                }
+                else
+                {
+                    ListNode *t1 = p, *t2 = q;
+                    p = p->next;
+                    q = q->next;
+                    last->next = t1;
+                    t1->next = t2;
+                    last = t2;
+                    last->next = nullptr;
+                }
+            }
+
+            if (p)
+            {
+                last->next = p;
+            }
+            if (q)
+            {
+                last->next = q;
+            }
+            return ans;
         }
-        
-        if(p)
-        {
-            last->next = p;
-        }
-        if(q)
-        {
-            last->next = q;
-        }        
-        return ans;
-    }
 };
