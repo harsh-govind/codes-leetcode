c8bf2c2a8b553926a298e152f37283422aeac27c
Harsh Govind <harsh5092001@gmail.com>
2022-08-02 21:29:18 +0530

    Time: 53 ms (72.78%), Space: 17.8 MB (77.50%) - LeetHub

--- 143-reorder-list/143-reorder-list.cpp
+++ 143-reorder-list/143-reorder-list.cpp
@@ -11,11 +11,11 @@
 class Solution
 {
     public:
-        ListNode *reverse(ListNode *q)
+        ListNode* reverse(ListNode *q)
         {
-            if(q==nullptr)
+            if (q == nullptr)
                 return q;
-            
+
             ListNode *x = q, *y = q, *z = nullptr;
             if (q)
             {
@@ -32,57 +32,41 @@ class Solution
             q = y;
             return q;
         }
-    
-        ListNode *mid(ListNode *head)
+
+    ListNode* mid(ListNode *head)
+    {
+        ListNode *p = head, *q = head, *r = head;;
+        while (p)
         {
-            ListNode *p = head, *q = head, *r=head;;
-            while (p)
+            p = p->next;
+            if (p)
             {
+                r = q;
                 p = p->next;
-                if (p)
-                {
-                    r=q;
-                    p = p->next;
-                    q = q->next;
-                }
-            }
-            r->next=nullptr;
-            return q;
-        }
-    
-        void print(ListNode *p)
-        {
-            while(p)
-            {
-                cout<<p->val<< " ";
-                p=p->next;
-            }
-            cout<<endl;
-        }
-        
-        void reorderList(ListNode *head)
-        {
-            ListNode *p = head, *q=mid(head), *ans=head;
-            print(p);
-            print(q);
-            q=reverse(q);
-            ListNode *t1, *t2;
-            while (p and q)
-            {
-                t1 = p;
-                p = p->next;
-                t2 = q;
                 q = q->next;
-                t1->next = t2;
-                t2->next = p;
             }
-            if (q)
-            {
-                t2->next = q;
-            } 
-            cout<<endl;
-            print(p);
-            print(q);
-            
         }
+        r->next = nullptr;
+        return q;
+    }
+
+    void reorderList(ListNode *head)
+    {
+        ListNode *p = head, *q = mid(head);
+        q = reverse(q);
+        ListNode *t1, *t2;
+        while (p and q)
+        {
+            t1 = p;
+            p = p->next;
+            t2 = q;
+            q = q->next;
+            t1->next = t2;
+            t2->next = p;
+        }
+        if (q)
+        {
+            t2->next = q;
+        }
+    }
 };
