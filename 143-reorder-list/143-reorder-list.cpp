da6f1973a3aefa5cd77052a308fa60d718f69bec
Harsh Govind <harsh5092001@gmail.com>
2022-08-02 21:27:57 +0530

    Time: 88 ms (23.65%), Space: 17.9 MB (50.05%) - LeetHub

--- 143-reorder-list/143-reorder-list.cpp
+++ 143-reorder-list/143-reorder-list.cpp
@@ -1,44 +1,88 @@
 /**
  *Definition for singly-linked list.
  *struct ListNode {
- *    int val;
- *    ListNode * next;
- *    ListNode() : val(0), next(nullptr) {}
- *    ListNode(int x) : val(x), next(nullptr) {}
- *    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *int val;
+ *ListNode * next;
+ *ListNode() : val(0), next(nullptr) {}
+ *ListNode(int x) : val(x), next(nullptr) {}
+ *ListNode(int x, ListNode *next) : val(x), next(next) {}
  *};
  */
 class Solution
 {
     public:
-        void reorderList(ListNode *head)
+        ListNode *reverse(ListNode *q)
         {
-            ListNode *p = head;
-            vector<int> k, k2;
+            if(q==nullptr)
+                return q;
+            
+            ListNode *x = q, *y = q, *z = nullptr;
+            if (q)
+            {
+                z = q->next;
+            }
+            while (z)
+            {
+                x = y;
+                y = z;
+                z = z->next;
+                y->next = x;
+            }
+            q->next = nullptr;
+            q = y;
+            return q;
+        }
+    
+        ListNode *mid(ListNode *head)
+        {
+            ListNode *p = head, *q = head, *r=head;;
             while (p)
             {
-                k.push_back(p->val);
                 p = p->next;
+                if (p)
+                {
+                    r=q;
+                    p = p->next;
+                    q = q->next;
+                }
             }
- 
-            for (int i = 0; i < k.size()/2; i++)
-            {
-                k2.push_back(k[i]);
-                k2.push_back(k[k.size()-i-1]);
-            }
-            p=head;
-            int i=0;
-            
-            if(k.size()%2!=0)
-            {
-                k2.push_back(k[k.size()/2]);
-            }
-            
+            r->next=nullptr;
+            return q;
+        }
+    
+        void print(ListNode *p)
+        {
             while(p)
             {
-                p->val=k2[i++];
+                cout<<p->val<< " ";
                 p=p->next;
             }
-
+            cout<<endl;
+        }
+        
+        void reorderList(ListNode *head)
+        {
+            ListNode *p = head, *q=mid(head), *ans=head;
+            print(p);
+            print(q);
+            q=reverse(q);
+            ListNode *t1, *t2;
+            while (p and q)
+            {
+                t1 = p;
+                p = p->next;
+                t2 = q;
+                q = q->next;
+                t1->next = t2;
+                t2->next = p;
+            }
+            if (q)
+            {
+                t2->next = q;
+            } 
+            cout<<endl;
+            print(p);
+            print(q);
+            
         }
 };
