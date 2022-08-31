26aff6655cb222f37d0331a655a84319d11af92f
Harsh Govind <harsh5092001@gmail.com>
2022-08-31 22:36:36 +0530

    Time: 29 ms (81.42%), Space: 31.1 MB (52.31%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -1,17 +1,17 @@
-class Solution {
-public:
-    int pivotIndex(vector<int>& nums) 
-    {
-        int totalSum = accumulate(nums.begin(), nums.end(), 0), leftSum=0;
-        for(int i=0;i<nums.size();i++)
+class Solution
+{
+    public:
+        int pivotIndex(vector<int> &nums)
         {
-            if(totalSum-nums[i]-leftSum==leftSum)
+            int totalSum = accumulate(nums.begin(), nums.end(), 0), leftSum = 0;
+            for (int i = 0; i < nums.size(); i++)
             {
-                return i;
+                if (totalSum - nums[i] - leftSum == leftSum)
+                {
+                    return i;
+                }
+                leftSum += nums[i];
             }
-            leftSum+=nums[i];
+            return -1;
         }
-        return -1;
-        
-    }
 };
