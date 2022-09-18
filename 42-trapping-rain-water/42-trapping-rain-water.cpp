2f3f2860e77c38329592ba99e7dcaff0ff3fbf83
Harsh Govind <harsh5092001@gmail.com>
2022-09-18 23:38:41 +0530

    Time: 43 ms (9.88%), Space: 20.7 MB (11.14%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int trap(vector<int> &v)
+{
+    int i, n = v.size(), ans = 0;
+    vector<int> left(n), right(n);
+    left[0]=v[0];
+    right[n - 1] = v[n - 1];
+    for (i = 1; i < n; i++)
+    {
+        left[i] = max(left[i - 1], v[i]);
+    }
+    for (i = n - 2; i >= 0; i--)
+    {
+        right[i] = max(right[i + 1], v[i]);
+    }
+    for (i = 0; i < n; i++)
+    {
+        ans += min(left[i], right[i]) - v[i];
+    }
+    return ans;
+}
+};
