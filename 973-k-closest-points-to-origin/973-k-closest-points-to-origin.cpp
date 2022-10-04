940654d5202b7a1368844ed93688e286b1abe884
Harsh Govind <harsh5092001@gmail.com>
2022-10-04 14:54:07 +0530

    Time: 257 ms (89.48%), Space: 69.7 MB (53.25%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,29 +1,35 @@
-class Solution {
-public:
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<int, pair<int, int>>> maxHeap;
-        
-        for(int i=0; i<points.size(); i++)
+class Solution
+{
+    public:
+        vector<vector < int>> kClosest(vector<vector < int>> &points, int k)
         {
-            int key = points[i][0]*points[i][0] + points[i][1]*points[i][1];
-            maxHeap.push({key, {points[i][0], points[i][1]}});
-            
-            if(maxHeap.size()>k)
+            priority_queue<pair<int, pair<int, int>>> maxHeap;
+
+            for (int i = 0; i < points.size(); i++)
             {
+                int key = points[i][0] *points[i][0] + points[i][1] *points[i][1];
+                maxHeap.push({ key,
+                    {
+                        points[i][0],
+                        points[i][1]
+                    } });
+
+                if (maxHeap.size() > k)
+                {
+                    maxHeap.pop();
+                }
+            }
+
+            vector<vector < int>> ans;
+
+            while (maxHeap.size() > 0)
+            {
+                vector<int> temp;
+                temp.push_back(maxHeap.top().second.first);
+                temp.push_back(maxHeap.top().second.second);
+                ans.push_back(temp);
                 maxHeap.pop();
             }
+            return ans;
         }
-        
-        vector<vector<int>> ans;
-        
-        while(maxHeap.size()>0)
-        {
-            vector<int> temp;
-            temp.push_back(maxHeap.top().second.first);
-            temp.push_back(maxHeap.top().second.second);
-            ans.push_back(temp);
-            maxHeap.pop();
-        }
-        return ans;
-    }
 };
