ceeb3d826d800bde68822e0a3b01dc9cd16523d4
Harsh Govind <harsh5092001@gmail.com>
2022-10-07 15:36:08 +0530

    Time: 14 ms (85.86%), Space: 9 MB (92.94%) - LeetHub

---
+++ 494-target-sum/494-target-sum.cpp
@@ -0,0 +1,44 @@
+class Solution
+{
+    public:
+        int perfectSum(vector<int> arr, int sum)
+        {
+            int i, j;
+            int n = arr.size();
+            int t[n + 1][sum + 1];
+            for (int i = 0; i < n + 1; i++)
+            {
+                for (int j = 0; j < sum + 1; j++)
+                {
+                    if (i == 0) t[i][j] = 0;
+                    if (j == 0) t[i][j] = 1;
+                }
+            }
+
+            for (i = 1; i < n + 1; i++)
+            {
+                for (j = 0; j < sum + 1; j++)
+                {
+                    if (arr[i - 1] <= j)
+                    {
+                        t[i][j] = (t[i - 1][j - arr[i - 1]] + t[i - 1][j]);
+                    }
+                    else
+                    {
+                        t[i][j] = (t[i - 1][j]);
+                    }
+                }
+            }
+            return t[n][sum];
+        }
+
+    int findTargetSumWays(vector<int> &nums, int target)
+    {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        int x = (sum - target) / 2;
+        if (((sum - target) % 2 == 1) || (target > sum))
+            return 0;
+        cout << x;
+        return perfectSum(nums, x);
+    }
+};
