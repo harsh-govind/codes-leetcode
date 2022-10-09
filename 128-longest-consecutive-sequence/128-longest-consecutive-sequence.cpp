b492be759d7f30234bce521efa0f0960737e9e66
Harsh Govind <harsh5092001@gmail.com>
2022-10-09 22:23:51 +0530

    Time: 986 ms (25.68%), Space: 49.4 MB (66.00%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,49 +1,33 @@
 class Solution
 {
     public:
-        vector<int> removeDuplicate(vector<int> &nums)
+
+        int longestConsecutive(vector<int> &k)
         {
-            vector<int> ans;
-            set<int> s;
-            for (auto &v: nums)
+            ios_base::sync_with_stdio(false);
+            cin.tie(NULL);
+
+            unordered_set<int> s;
+
+            for (auto &v: k)
             {
                 s.insert(v);
             }
-            for (auto &v: s)
+            int ans = 0, currAns = 0;
+            for (auto &v: k)
             {
-                ans.push_back(v);
+                currAns = 1;
+                if (s.find(v - 1) == s.end())
+                {
+                    int temp = v;
+                    while (s.find(temp+1) != s.end())
+                    {
+                        currAns++;
+                        temp++;
+                    }
+                    ans = max(ans, currAns);
+                }
             }
             return ans;
         }
-    int longestConsecutive(vector<int> &k)
-    {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        vector<int> nums = removeDuplicate(k);
-        int maxCount = 0, currentCount = 0;
-        if (nums.size() == 0)
-            return 0;
-        if (nums.size() == 1)
-            return 1;
-        for (int i = 1; i < nums.size(); i++)
-        {
-            if (nums[i - 1] + 1 == nums[i])
-            {
-                if (currentCount == 0)
-                {
-                    currentCount++;
-                }
-                currentCount++;
-            }
-            else
-            {
-                maxCount = max(currentCount, maxCount);
-                currentCount = 0;
-            }
-        }
-        maxCount = max(currentCount, maxCount);
-        if (maxCount == 0)
-            maxCount++;
-        return maxCount;
-    }
 };
