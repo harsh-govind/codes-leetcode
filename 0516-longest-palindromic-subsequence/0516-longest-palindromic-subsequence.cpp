81bc156128f23cded54581e10954dc59eb8ef920
Harsh Govind <harsh5092001@gmail.com>
2022-10-29 18:33:05 +0530

    Time: 98 ms (88.26%), Space: 10.9 MB (69.98%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,34 +1,36 @@
-class Solution {
-public:
-    int longestPalindromeSubseq(string a) {
-        string b=a;
-        reverse(b.begin(), b.end());
-        int m=a.size(), n=b.size();
-        int t[m+1][n+1];
-        for(int i=0;i<m+1;i++)
+class Solution
+{
+    public:
+        int longestPalindromeSubseq(string a)
         {
-            t[i][0]=0;
-        }
-        for(int i=0; i<n+1;i++)
-        {
-            t[0][i]=0;
-        }
-        
-        for(int i=1;i<m+1; i++)
-        {
-            for(int j=1; j<n+1; j++)
+            string b = a;
+            reverse(b.begin(), b.end());
+            int m = a.size(), n = b.size();
+            int t[m + 1][n + 1];
+            for (int i = 0; i < m + 1; i++)
             {
-                if(a[i-1]==b[j-1])
+                t[i][0] = 0;
+            }
+            for (int i = 0; i < n + 1; i++)
+            {
+                t[0][i] = 0;
+            }
+
+            for (int i = 1; i < m + 1; i++)
+            {
+                for (int j = 1; j < n + 1; j++)
                 {
-                    t[i][j]=1+t[i-1][j-1];
-                }
-                else
-                {
-                    t[i][j]=max(t[i-1][j], t[i][j-1]);
+                    if (a[i - 1] == b[j - 1])
+                    {
+                        t[i][j] = 1 + t[i - 1][j - 1];
+                    }
+                    else
+                    {
+                        t[i][j] = max(t[i - 1][j], t[i][j - 1]);
+                    }
                 }
             }
+
+            return t[m][n];
         }
-        
-        return t[m][n];
-    }
 };
