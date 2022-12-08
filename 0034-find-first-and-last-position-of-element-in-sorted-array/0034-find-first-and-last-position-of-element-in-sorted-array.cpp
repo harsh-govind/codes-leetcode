fd55d7cd2078572363b9506760fda91fb1f91fa3
Harsh Govind <harsh5092001@gmail.com>
2022-12-08 09:50:30 +0530

    Time: 8 ms (91.04%), Space: 13.6 MB (85.19%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -6,7 +6,6 @@ class Solution
             vector<int> ans;
             if (v.size() == 0)
             {
-                cout << "first" << endl;
                 ans.push_back(-1);
                 ans.push_back(-1);
                 return ans;
@@ -14,16 +13,12 @@ class Solution
             auto it = lower_bound(v.begin(), v.end(), target);
             if (it == v.end())
             {
-                cout << "second" << endl;
-
                 ans.push_back(-1);
                 ans.push_back(-1);
                 return ans;
             }
             else if (*it != target)
             {
-                cout << "third" << endl;
-
                 ans.push_back(-1);
                 ans.push_back(-1);
                 return ans;
@@ -31,21 +26,15 @@ class Solution
             auto it2 = upper_bound(v.begin(), v.end(), target);
             it2--;
 
-            if (it2 != v.begin()-1)
+            if (it2 != v.begin() - 1)
             {
-                cout << "fourth" << endl;
-
                 if (*it2 == target)
                 {
-                    cout << "fifth" << endl;
-
                     ans.push_back(it - v.begin());
                     ans.push_back(it2 - v.begin());
                     return ans;
                 }
             }
-            cout << "sixth" << endl;
-
             ans.push_back(-1);
             ans.push_back(-1);
             return ans;
