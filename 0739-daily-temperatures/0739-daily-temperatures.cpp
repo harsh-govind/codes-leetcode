e271d473d331be8d37c0f47dd6432bb873af0000
Harsh Govind <harsh5092001@gmail.com>
2022-12-09 21:04:25 +0530

    Time: 241 ms (80.09%), Space: 98.7 MB (66.03%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,40 @@
+class Solution
+{
+    public:
+        vector<int> dailyTemperatures(vector<int> &t)
+        {
+
+            stack<int> stk;
+            vector<int> ans(t.size(), 0);
+
+            for (int i = t.size() - 1; i >= 0; i--)
+            {
+                if (stk.empty())
+                {
+                    stk.push(i);
+                    ans[i] = 0;
+                }
+                else
+                {
+                    
+                        while (!stk.empty() and t[stk.top()] <= t[i])
+                        {
+                            stk.pop();
+                        }
+
+                        if (stk.size() == 0)
+                        {
+                            ans[i] = 0;
+                            stk.push(i);
+                        }
+                        else
+                        {
+                            ans[i] = stk.top() - i;
+                            stk.push(i);
+                        }
+                    
+                }
+            }
+            return ans;
+        }
+};
