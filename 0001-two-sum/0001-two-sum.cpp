b1500a20bebbd59fb3a97583f19c71dea963e2cd
Harsh Govind <harsh5092001@gmail.com>
2022-12-12 10:32:56 +0530

    Time: 28 ms (64.76%), Space: 10.9 MB (43.79%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+    public:
+        vector<int> twoSum(vector<int> &v, int key)
+        {
+            
+            unordered_map<int, int> s;
+            int n = v.size();
+            vector<int> ans;
+            for (int i = 0; i < n; i++)
+            {
+                if (s.find(key - v[i]) != s.end())
+                {
+                    ans.push_back(i);
+                    ans.push_back(s[key - v[i]]);
+                    return ans;
+                }
+                s[v[i]] = i;
+            }
+            return ans;
+        }
+};
