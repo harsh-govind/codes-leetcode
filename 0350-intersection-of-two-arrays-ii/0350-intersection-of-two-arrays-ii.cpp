a014cb1e1bcbe54b03d3c533448aa0b446e47a17
Harsh Govind <harsh5092001@gmail.com>
2022-12-13 10:47:03 +0530

    Time: 5 ms (86.79%), Space: 10 MB (93.54%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -26,6 +26,8 @@ class Solution
         }
     vector<int> intersect(vector<int> &v1, vector<int> &v2)
     {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         sort(v1.begin(), v1.end());
         sort(v2.begin(), v2.end());
         vector<int> ans = intersectionMerge(v1, v2);
