60576dd2a0ce5ae3d991669dc1186cfda1e2f5a6
Harsh Govind <harsh5092001@gmail.com>
2022-12-16 03:15:02 +0530

    Time: 25 ms (86.98%), Space: 20.7 MB (13.04%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,84 @@
+class Solution
+{
+    public:
+        bool validateSubboard(vector<vector < char>> &board)
+        {
+            for (int i = 0; i < 9; i += 3)
+            {
+                for (int j = 0; j < 9; j += 3)
+                {
+                    unordered_map<char, int> m;
+                    for (int a = i; a < i + 3; a++)
+                    {
+                        for (int b = j; b < j + 3; b++)
+                        {
+                            if (!((board[a][b] >= '1'
+                                    and board[a][b] <= '9') or board[a][b] == '.'))
+                            {
+                                return false;
+                            }
+                            m[board[a][b]]++;
+                        }
+                    }
+                    for (auto &val: m)
+                    {
+                        if (val.first != '.'
+                            and val.second > 1)
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+    bool validRow(vector<vector < char>> &board)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            unordered_map<char, int> m;
+
+            for (int j = 0; j < 9; j++)
+            {
+                m[board[i][j]]++;
+            }
+            for (auto &val: m)
+            {
+                if (val.first != '.'
+                    and val.second > 1)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    bool validColumn(vector<vector < char>> &board)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            unordered_map<char, int> m;
+
+            for (int j = 0; j < 9; j++)
+            {
+                m[board[j][i]]++;
+            }
+            for (auto &val: m)
+            {
+                if (val.first != '.'
+                    and val.second > 1)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    bool isValidSudoku(vector<vector < char>> &board)
+    {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        return validColumn(board) and validRow(board) and validateSubboard(board);
+    }
+};
