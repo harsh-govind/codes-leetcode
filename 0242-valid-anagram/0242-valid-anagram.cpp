1823519ff059f25ee9cd3af807f83f48a5f276bd
Harsh Govind <harsh5092001@gmail.com>
2022-12-16 17:46:28 +0530

    Time: 9 ms (89.35%), Space: 7.2 MB (82.90%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+    public:
+        bool isAnagram(string a, string b)
+        {
+            unordered_map<char, int> ma, mb;
+            for (int i = 0; i < a.size(); i++)
+            {
+                ma[a[i]]++;
+            }
+            for (int i = 0; i < b.size(); i++)
+            {
+                mb[b[i]]++;
+            }
+            return ma==mb;
+        }
+};
