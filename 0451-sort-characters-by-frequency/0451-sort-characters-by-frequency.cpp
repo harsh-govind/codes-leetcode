e138a84a942e156fba4074b93aede4c7e9a85561
Harsh Govind <harsh5092001@gmail.com>
2022-12-16 17:26:47 +0530

    Time: 18 ms (71.57%), Space: 8.6 MB (45.92%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+    public:
+        string frequencySort(string s)
+        {
+            unordered_map<char, int> arr;
+            for (int i = 0; i < s.size(); i++)
+            {
+                arr[s[i]]++;
+            }
+            multimap<int, char, greater < int>> fn;
+            for (auto &val: arr)
+            {
+                cout << val.first << " " << val.second << endl;
+                fn.insert(make_pair(val.second, val.first));
+            }
+            string ans = "";
+            for (auto &val: fn)
+            {
+                for (int i = 0; i < val.first; i++)
+                {
+                    ans += val.second;
+                }
+            }
+            return ans;
+        }
+};
