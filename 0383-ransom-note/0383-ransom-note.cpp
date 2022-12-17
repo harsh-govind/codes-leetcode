98d2e152fd800af04338d0b9df3bcd2fc26ba3ab
Harsh Govind <harsh5092001@gmail.com>
2022-12-17 20:24:04 +0530

    Time: 11 ms (95.87%), Space: 8.8 MB (72.73%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -4,16 +4,16 @@ class Solution
         bool canConstruct(string a, string b)
         {
            	//return true if a can ve contructed by using b
-            int am[2556] = { 0 };
+            int am[26] = { 0 };
 
             for (int i = 0; i < b.size(); i++)
             {
-                am[b[i]]++;
+                am[b[i] - 'a']++;
             }
             for (int i = 0; i < a.size(); i++)
             {
-                if (am[a[i]] <= 0) return false;
-                am[a[i]]--;
+                if (am[a[i] - 'a'] <= 0) return false;
+                am[a[i] - 'a']--;
             }
 
             return true;
