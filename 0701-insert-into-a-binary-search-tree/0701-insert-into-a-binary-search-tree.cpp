47cd84c277962869fa02831e063a29baabc1ded9
Harsh Govind <harsh5092001@gmail.com>
2022-12-24 13:12:05 +0530

    Time: 88 ms (97.89%), Space: 57 MB (51.84%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
@@ -1,47 +1,49 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    TreeNode* insertIntoBST(TreeNode* root, int val) {
-        if(!root)
+class Solution
+{
+    public:
+        TreeNode* insertIntoBST(TreeNode *root, int val)
         {
-            root=new TreeNode(val);
-            return root;
-        }
-        TreeNode *p=root, *prev=root;
-        while(p)
-        {
-            if(p->val>val)
+            if (!root)
             {
-                prev=p;
-                p=p->left;
+                root = new TreeNode(val);
+                return root;
+            }
+            TreeNode *p = root, *prev = root;
+            while (p)
+            {
+                if (p->val > val)
+                {
+                    prev = p;
+                    p = p->left;
+                }
+                else
+                {
+                    prev = p;
+                    p = p->right;
+                }
+            }
+
+            cout << prev->val;
+            TreeNode *k = new TreeNode(val);
+            if (prev->val > val)
+            {
+                prev->left = k;
             }
             else
             {
-                prev=p;
-                p=p->right;
+                prev->right = k;
             }
+            return root;
         }
-        
-        cout<<prev->val;
-        TreeNode *k=new TreeNode(val);
-        if(prev->val > val)
-        {
-            prev->left=k;
-        }
-        else
-        {
-            prev->right=k;
-        }
-        return root;
-    }
 };
