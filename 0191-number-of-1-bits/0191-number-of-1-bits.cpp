fb389e484165d43e8f172d5396b7fa819e137ebc
Harsh Govind <harsh5092001@gmail.com>
2022-12-28 20:37:55 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (13.32%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,14 +1,19 @@
 class Solution
 {
     public:
-        int hammingWeight(uint32_t n)
+        Solution()
         {
-            int ans = 0;
-            while (n > 0)
-            {
-                ans += n &1;
-                n = n >> 1;
-            }
-            return ans;
+            ios_base::sync_with_stdio(false);
+            cin.tie(NULL);
         }
+    int hammingWeight(uint32_t n)
+    {
+        int ans = 0;
+        while (n > 0)
+        {
+            ans += n &1;
+            n = n >> 1;
+        }
+        return ans;
+    }
 };
