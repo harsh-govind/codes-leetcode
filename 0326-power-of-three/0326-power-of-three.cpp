916f3bcdf6aae7adf5cdc16890677407b049ed95
Harsh Govind <harsh5092001@gmail.com>
2022-12-28 21:21:38 +0530

    Time: 152 ms (5.21%), Space: 44.4 MB (9.64%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -3,12 +3,11 @@ class Solution
     public:
         bool isPowerOfThree(int n)
         {
-            if(n<0)
+            if (n < 0)
                 return false;
-            if(n==1)
-                return true;
             set < long long > s;
             long long check = 1;
+            s.insert(1);
             for (int i = 0; i <= 21; i++)
             {
                 s.insert(check *= 3);
