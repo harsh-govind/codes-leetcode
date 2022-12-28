c8a013f1d56523b34efcae26b4bcdbd7b11d77b1
Harsh Govind <harsh5092001@gmail.com>
2022-12-28 21:22:52 +0530

    Time: 126 ms (5.21%), Space: 39.3 MB (9.64%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,17 +1,22 @@
 class Solution
 {
     public:
-        bool isPowerOfThree(int n)
+        Solution()
         {
-            if (n < 0)
-                return false;
-            set < long long > s;
-            long long check = 1;
-            s.insert(1);
-            for (int i = 0; i <= 18; i++)
-            {
-                s.insert(check *= 3);
-            }
-            return s.find(n) != s.end();
+            ios_base::sync_with_stdio(false);
+            cin.tie(NULL);
         }
+    bool isPowerOfThree(int n)
+    {
+        if (n < 0)
+            return false;
+        set < long long > s;
+        long long check = 1;
+        s.insert(1);
+        for (int i = 0; i <= 18; i++)
+        {
+            s.insert(check *= 3);
+        }
+        return s.find(n) != s.end();
+    }
 };
