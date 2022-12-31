460b28f3b87c44ac301b4d282d0dea3c89cae0f9
Harsh Govind <harsh5092001@gmail.com>
2022-12-31 23:44:08 +0530

    Time: 620 ms (34.50%), Space: 162.4 MB (7.80%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.cpp
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.cpp
@@ -1,30 +1,33 @@
 typedef long long ll;
 
-class Solution {
-public:
-    int shortestSubarray(vector<int>& v, int k) {
-        int ans = INT_MAX, n = v.size();
-        ll sumSoFar = 0;
-        
-        map<ll,int> prefSumMap;
-        prefSumMap[0] = -1;
-        
-        for(int i=0; i<n; ++i) {
-            sumSoFar += v[i];
-            prefSumMap[sumSoFar] = i;
-            
-            auto itUB = prefSumMap.upper_bound(sumSoFar-k);
-            
-            if(itUB == prefSumMap.begin()) continue;
-            
-            for(auto it = prefSumMap.begin(); it != itUB; ++it) {
-                ans = min(ans, (i-it->second));
+class Solution
+{
+    public:
+        int shortestSubarray(vector<int> &v, int k)
+        {
+            int ans = INT_MAX, n = v.size();
+            ll sumSoFar = 0;
+
+            map<ll, int> prefSumMap;
+            prefSumMap[0] = -1;
+
+            for (int i = 0; i < n; ++i)
+            {
+                sumSoFar += v[i];
+                prefSumMap[sumSoFar] = i;
+
+                auto itUB = prefSumMap.upper_bound(sumSoFar - k);
+
+                if (itUB == prefSumMap.begin()) continue;
+
+                for (auto it = prefSumMap.begin(); it != itUB; ++it)
+                {
+                    ans = min(ans, (i - it->second));
+                }
+
+                prefSumMap.erase(prefSumMap.begin(), itUB);
             }
-            
-            prefSumMap.erase(prefSumMap.begin(), itUB);
-            
+
+            return ans == INT_MAX ? -1 : ans;
         }
-        
-        return ans == INT_MAX ? -1 : ans;
-    }
 };
