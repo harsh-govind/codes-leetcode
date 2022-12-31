3e60d99aaeb69ec00b7c13bdc63ad4302c7adf4a
Harsh Govind <harsh5092001@gmail.com>
2023-01-01 00:38:03 +0530

    Time: 38 ms (87.80%), Space: 16.2 MB (80.25%) - LeetHub

---
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+    public:
+        bool checkXMatrix(vector<vector < int>> &grid)
+        {
+            int m = grid.size();
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (i == j or i + j == m-1)
+                    {
+                        if (grid[i][j] == 0)
+                        {
+                            cout<<"first";
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        if (grid[i][j] != 0)
+                        {
+                            
+                            cout<<"second"<<" "<<i<<" "<<j;
+
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+};
