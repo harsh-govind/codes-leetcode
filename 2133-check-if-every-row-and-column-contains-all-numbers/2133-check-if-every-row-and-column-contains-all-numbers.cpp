5aa964f17ceed600fd835ccb6bdea21a9a01f9aa
Harsh Govind <harsh5092001@gmail.com>
2023-01-01 17:41:25 +0530

    Time: 347 ms (51.37%), Space: 91.3 MB (35.50%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -17,11 +17,8 @@ class Solution
                     sr.insert(m[i][j]);
                     sc.insert(m[j][i]);
                 }
-                if(sr.size() !=n or sc.size()!=n)
-                {
-                    return false;
-                }
-                if ((sumr != ((n *(n + 1)) / 2)) or(sumc != ((n *(n + 1)) / 2)))
+ 
+                if ((sumr != ((n *(n + 1)) / 2)) or(sumc != ((n *(n + 1)) / 2)) or sr.size() != n or sc.size() != n)
                 {
                     return false;
                 }
