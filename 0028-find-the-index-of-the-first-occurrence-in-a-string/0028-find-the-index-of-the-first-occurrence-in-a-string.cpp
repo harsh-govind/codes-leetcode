604175255dda31225e04126eee29090286054bfd
Harsh Govind <harsh5092001@gmail.com>
2023-01-01 17:19:22 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (11.62%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+    public:
+        int strStr(string hay, string nee)
+        {
+            ios_base::sync_with_stdio(false);
+            cin.tie(NULL);
+            cout.tie(NULL);
+            if (hay.size() < nee.size())
+            {
+                return -1;
+            }
+            else if (hay == nee)
+                return 0;
+            int n = hay.size(), m = nee.size();
+
+            for (int i = 0; i < hay.size() - m + 1; i++)
+            {
+                string a = string(hay.begin() + i, hay.begin() + m + i);
+                if (a == nee) return i;
+            }
+
+            return -1;
+        }
+};
