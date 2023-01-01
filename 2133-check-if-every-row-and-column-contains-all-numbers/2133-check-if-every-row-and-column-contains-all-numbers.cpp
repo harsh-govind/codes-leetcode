912bff63018e1c48ec57e39a5e158996631afacd
Harsh Govind <harsh5092001@gmail.com>
2023-01-01 17:42:45 +0530

    Time: 292 ms (72.04%), Space: 91.4 MB (35.50%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -1,29 +1,34 @@
 class Solution
 {
     public:
-        bool checkValid(vector<vector < int>> &m)
+        Solution()
         {
-           	//row
-            int n = m.size();
-            cout << "row sums\n";
-            for (int i = 0; i < n; i++)
+            ios_base::sync_with_stdio(false);
+            cin.tie(NULL);
+        }
+    bool checkValid(vector<vector < int>> &m)
+    {
+       	//row
+        int n = m.size();
+        cout << "row sums\n";
+        for (int i = 0; i < n; i++)
+        {
+            int sumr = 0, sumc = 0;
+            unordered_set<int> sr, sc;
+            for (int j = 0; j < n; j++)
             {
-                int sumr = 0, sumc = 0;
-                unordered_set<int> sr, sc;
-                for (int j = 0; j < n; j++)
-                {
-                    sumr += m[i][j];
-                    sumc += m[j][i];
-                    sr.insert(m[i][j]);
-                    sc.insert(m[j][i]);
-                }
- 
-                if ((sumr != ((n *(n + 1)) / 2)) or(sumc != ((n *(n + 1)) / 2)) or sr.size() != n or sc.size() != n)
-                {
-                    return false;
-                }
+                sumr += m[i][j];
+                sumc += m[j][i];
+                sr.insert(m[i][j]);
+                sc.insert(m[j][i]);
             }
 
-            return true;
+            if ((sumr != ((n *(n + 1)) / 2)) or(sumc != ((n *(n + 1)) / 2)) or sr.size() != n or sc.size() != n)
+            {
+                return false;
+            }
         }
+
+        return true;
+    }
 };
