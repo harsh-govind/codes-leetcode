af5a1754d7be227897850f041873730b9bc85b54
Harsh Govind <harsh5092001@gmail.com>
2023-01-02 22:55:41 +0530

    Time: 0 ms (100.00%), Space: 6 MB (5.37%) - LeetHub

--- 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.cpp
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.cpp
@@ -1,26 +1,21 @@
 class Solution
 {
     public:
-        bool iseven(int num)
+        int countEven(int num)
         {
-            int sum = 0;
+            int temp = num, sum = 0;
             while (num > 0)
             {
                 sum += num % 10;
                 num /= 10;
             }
-            return (sum % 2) == 0;
+            return sum % 2 == 0 ? temp / 2 : (temp - 1) / 2;
         }
-    int countEven(int num)
-    {
-        int ans = 0;
-        for (int i = 1; i <= num; i++)
-        {
-            if (iseven(i))
-            {
-                ans++;
-            }
-        }
-        return ans;
-    }
 };
+/*
+We calculate the sum, and then apply formula (we just need to check if the sum is even or odd) to calculate the final answer.
+I got the idea while checking the given examples in the description. Just try to check what will be the ouptut for 10, 20, 30, 40 etc:
+10 = 2, 4, 6, 8 (4)
+20 = 2, 4, 6, 8, 11, 13, 15, 17, 19, 20 (10)
+30 = 2, 4, 6, 8, 11, 13, 15, 17, 19, 20, 22, 24, 26, and 28. (14)
+*/
