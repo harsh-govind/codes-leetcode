8338d56ddf5cba22442eb2e6b8523f22b3aef722
Harsh Govind <harsh5092001@gmail.com>
2023-01-04 23:10:13 +0530

    Time: 4 ms (69.84%), Space: 7.9 MB (32.95%) - LeetHub

---
+++ 2451-odd-string-difference/2451-odd-string-difference.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string oddString(vector<string>& words) {
+        map<vector<int>, int> mp;
+        
+        for(auto &word:words)
+        {
+            vector<int> temp;
+            for(int i=0; i<word.size()-1;i++)
+            {
+                temp.push_back((word[i+1]-'a')-(word[i]-'a'));
+            }
+            
+            mp[temp]++;
+        }
+        vector<int> key;
+        for(auto &val:mp)
+        {
+            cout<<val.second<<" ";
+            if(val.second==1)
+            {
+                key=val.first;
+            }
+        }
+         for(auto &word:words)
+        {
+            vector<int> temp;
+            for(int i=0; i<word.size()-1;i++)
+            {
+                temp.push_back((word[i+1]-'a')-(word[i]-'a'));
+            }
+            
+            if(temp==key)
+            {
+                return word;
+            }
+        }
+        return "";
+        
+    }
+};
