d9cfd6b5625c74036134d223039ed7fc062ba4be
Harsh Govind <harsh5092001@gmail.com>
2023-01-04 23:10:31 +0530

    Time: 4 ms (69.84%), Space: 7.9 MB (32.95%) - LeetHub

--- 2451-odd-string-difference/2451-odd-string-difference.cpp
+++ 2451-odd-string-difference/2451-odd-string-difference.cpp
@@ -1,41 +1,42 @@
-class Solution {
-public:
-    string oddString(vector<string>& words) {
-        map<vector<int>, int> mp;
-        
-        for(auto &word:words)
+class Solution
+{
+    public:
+        string oddString(vector<string> &words)
         {
-            vector<int> temp;
-            for(int i=0; i<word.size()-1;i++)
+            map<vector < int>, int> mp;
+
+            for (auto &word: words)
             {
-                temp.push_back((word[i+1]-'a')-(word[i]-'a'));
+                vector<int> temp;
+                for (int i = 0; i < word.size() - 1; i++)
+                {
+                    temp.push_back((word[i + 1] - 'a') - (word[i] - 'a'));
+                }
+
+                mp[temp]++;
             }
-            
-            mp[temp]++;
+            vector<int> key;
+            for (auto &val: mp)
+            {
+                cout << val.second << " ";
+                if (val.second == 1)
+                {
+                    key = val.first;
+                }
+            }
+            for (auto &word: words)
+            {
+                vector<int> temp;
+                for (int i = 0; i < word.size() - 1; i++)
+                {
+                    temp.push_back((word[i + 1] - 'a') - (word[i] - 'a'));
+                }
+
+                if (temp == key)
+                {
+                    return word;
+                }
+            }
+            return "";
         }
-        vector<int> key;
-        for(auto &val:mp)
-        {
-            cout<<val.second<<" ";
-            if(val.second==1)
-            {
-                key=val.first;
-            }
-        }
-         for(auto &word:words)
-        {
-            vector<int> temp;
-            for(int i=0; i<word.size()-1;i++)
-            {
-                temp.push_back((word[i+1]-'a')-(word[i]-'a'));
-            }
-            
-            if(temp==key)
-            {
-                return word;
-            }
-        }
-        return "";
-        
-    }
 };
