7ccbd7c1edb701523506a48beffaa0ad717ee268
Harsh Govind <harsh5092001@gmail.com>
2023-01-23 23:30:39 +0530

    Time: 33 ms (12.08%), Space: 10.7 MB (79.43%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+    public:
+        string destCity(vector<vector < string>> &paths)
+        {
+            set<string> s;
+
+            for (auto &val: paths)
+            {
+                s.insert(val[0]);
+            }
+
+            for (auto &val: paths)
+            {
+                if (s.find(val[1]) == s.end())
+                {
+                    return val[1];
+                }
+            }
+            return "";
+        }
+};
