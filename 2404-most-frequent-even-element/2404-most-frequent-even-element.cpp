76caa2b151b2a1e1613beb8e968e29d164e8c5cd
Harsh Govind <harsh5092001@gmail.com>
2023-01-29 09:55:16 +0530

    Time: 58 ms (99.24%), Space: 38.9 MB (35.12%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
@@ -1,31 +1,35 @@
-class Solution {
-public:
-    int mostFrequentEven(vector<int>& nums) {
-        map<int, int> m;
-        
-        for(auto &v:nums)
+class Solution
+{
+    public:
+        int mostFrequentEven(vector<int> &nums)
         {
-            if(v%2==0)
+            std::ios::sync_with_stdio(false);
+            std::cin.tie(nullptr);
+            map<int, int> m;
+
+            for (auto &v: nums)
             {
-                m[v]++;
+                if (v % 2 == 0)
+                {
+                    m[v]++;
+                }
             }
-        }
-        
-        if(m.size()==0)
-        {
-            return -1;
-        }
-        
-        int ans=INT_MIN, k=INT_MIN;
-        
-        for(auto &val:m)
-        {
-            if(val.second > k)
+
+            if (m.size() == 0)
             {
-                k=val.second;
-                ans=val.first;
+                return -1;
             }
+
+            int ans = INT_MIN, k = INT_MIN;
+
+            for (auto &val: m)
+            {
+                if (val.second > k)
+                {
+                    k = val.second;
+                    ans = val.first;
+                }
+            }
+            return ans;
         }
-        return ans;
-    }
 };
