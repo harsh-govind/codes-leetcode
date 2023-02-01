9b105353dd5926dc4d1c0cf81ecb8b8a50ce6e9e
Harsh Govind <harsh5092001@gmail.com>
2023-02-01 20:10:22 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (16.50%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.cpp
+++ 0500-keyboard-row/0500-keyboard-row.cpp
@@ -1,44 +1,69 @@
 class Solution
 {
-public:
-    vector<string> findWords(vector<string> &words)
-    {
-        unordered_set<char> row1 = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'}, row2 = {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'}, row3 = {'z', 'x', 'c', 'v', 'b', 'n', 'm'};
-
-        vector<string> ans;;
-
-        for (string &word : words)
+    public:
+        vector<string> findWords(vector<string> &words)
         {
-            int r = 0;
-            if (row1.count(tolower(word[0])))
-                r = 1;
-            else if (row2.count(tolower(word[0])))
-                r = 2;
-            else
-                r = 3;
+            std::ios::sync_with_stdio(false);
+            std::cin.tie(nullptr);
+            unordered_set<char> row1 = { 'q',
+                'w',
+                'e',
+                'r',
+                't',
+                'y',
+                'u',
+                'i',
+                'o',
+                'p' }, row2 = { 'a',
+                's',
+                'd',
+                'f',
+                'g',
+                'h',
+                'j',
+                'k',
+                'l' }, row3 = { 'z',
+                'x',
+                'c',
+                'v',
+                'b',
+                'n',
+                'm' };
 
-            bool flag = true;
-            for (char c : word)
+            vector<string> ans;;
+
+            for (string &word: words)
             {
-                if (r == 1 && !row1.count(tolower(c)))
+                int r = 0;
+                if (row1.count(tolower(word[0])))
+                    r = 1;
+                else if (row2.count(tolower(word[0])))
+                    r = 2;
+                else
+                    r = 3;
+
+                bool flag = true;
+                for (char c: word)
                 {
-                    flag = false;
-                    break;
+                    if (r == 1 && !row1.count(tolower(c)))
+                    {
+                        flag = false;
+                        break;
+                    }
+                    else if (r == 2 && !row2.count(tolower(c)))
+                    {
+                        flag = false;
+                        break;
+                    }
+                    else if (r == 3 && !row3.count(tolower(c)))
+                    {
+                        flag = false;
+                        break;
+                    }
                 }
-                else if (r == 2 && !row2.count(tolower(c)))
-                {
-                    flag = false;
-                    break;
-                }
-                else if (r == 3 && !row3.count(tolower(c)))
-                {
-                    flag = false;
-                    break;
-                }
+                if (flag)
+                    ans.push_back(word);
             }
-            if (flag)
-                ans.push_back(word);
+            return ans;
         }
-        return ans;
-    }
 };
