a24af55a9ee513bf791538eb4fd49f897c45af71
Harsh Govind <harsh5092001@gmail.com>
2023-02-06 19:02:43 +0530

    Time: 6 ms (63.25%), Space: 9.9 MB (31.98%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -3,6 +3,8 @@ class Solution
     public:
         vector<int> shuffle(vector<int> &nums, int n)
         {
+            std::ios::sync_with_stdio(false);
+            std::cin.tie(nullptr);
             vector<int> v;
             int j = (n);
             for (int i = 0; i < n; i++)
