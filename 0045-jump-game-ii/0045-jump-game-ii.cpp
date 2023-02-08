7bd665f32500747975e71b3173ba6c3d6dd2f9bf
Harsh Govind <harsh5092001@gmail.com>
2023-02-08 23:53:46 +0530

    Time: 11 ms (95.23%), Space: 16.6 MB (84.26%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+    public:
+        int jump(vector<int> &nums)
+        {
+            int curr_reach = 0, max_reach = 0, jumps = 0;
+            for (int i = 0; i < nums.size() - 1; i++)
+            {
+                max_reach = max(max_reach, i + nums[i]);
+                if (i == curr_reach)
+                {
+                    curr_reach = max_reach;
+                    jumps++;
+                }
+            }
+            return jumps;
+        }
+};
