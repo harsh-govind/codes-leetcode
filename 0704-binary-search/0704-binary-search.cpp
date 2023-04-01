b2d9a55cdc495555020c78566baa51d529e5af60
Harsh Govind <harsh5092001@gmail.com>
2023-04-01 22:12:27 +0530

    Time: 35 ms (83.54%), Space: 27.4 MB (93.53%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,25 +1,27 @@
-class Solution {
-public:
-    int search(vector<int>& nums, int target) {
-        int l=0, h=nums.size()-1, m=0;
-        
-        while(l<=h)
+class Solution
+{
+    public:
+        int search(vector<int> &nums, int target)
         {
-            m=(l+h)/2;
-            if(nums[m]==target)
+            int l = 0, h = nums.size() - 1, m = 0;
+
+            while (l <= h)
             {
-                return m;
+                m = (l + h) / 2;
+                if (nums[m] == target)
+                {
+                    return m;
+                }
+                else if (nums[m] < target)
+                {
+                    l = m + 1;
+                }
+                else
+                {
+                    h = m - 1;
+                }
             }
-            else if(nums[m]<target)
-            {
-                l=m+1;
-            }
-            else
-            {
-                h=m-1;
-            }
+
+            return -1;
         }
-        
-        return -1;
-    }
 };
