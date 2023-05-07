d342c25e3729c872031405a3206a3fdb625483ac
Harsh Govind <harsh5092001@gmail.com>
2023-05-07 16:06:03 +0530

    Time: 47 ms (30.00%), Space: 20 MB (33.47%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,25 +1,27 @@
-class Solution {
-public:
-    vector<vector<int>> merge(vector<vector<int>>& arr) {
-        sort(arr.begin(), arr.end());
-        int n = arr.size(), i=0;
-        vector<vector<int>> ans;
-        while(i<n)
+class Solution
+{
+    public:
+        vector<vector < int>> merge(vector<vector < int>> &arr)
         {
-            int low=arr[i][0], high=arr[i][1];
-            i++;
-            while(i<n and arr[i][0]>=low and arr[i][0]<=high )
+            sort(arr.begin(), arr.end());
+            int n = arr.size(), i = 0;
+            vector<vector < int>> ans;
+            while (i < n)
             {
-                high=max(high,arr[i][1]);
+                int low = arr[i][0], high = arr[i][1];
                 i++;
+                while (i < n and arr[i][0] >= low and arr[i][0] <= high)
+                {
+                    high = max(high, arr[i][1]);
+                    i++;
+                }
+
+                vector<int> temp;
+                temp.push_back(low);
+                temp.push_back(high);
+                ans.push_back(temp);
             }
-            
-            vector<int> temp;
-            temp.push_back(low);
-            temp.push_back(high);
-            ans.push_back(temp);
+
+            return ans;
         }
-        
-        return ans;
-    }
 };
