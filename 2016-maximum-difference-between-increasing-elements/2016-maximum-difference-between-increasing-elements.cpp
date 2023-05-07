cefdd17e9627f1fdfd4bc71faf8e9630aa1eef05
Harsh Govind <harsh5092001@gmail.com>
2023-05-07 21:11:43 +0530

    Time: 3 ms (83.80%), Space: 8.3 MB (43.97%) - LeetHub

---
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maximumDifference(vector<int>& prices) {
+        ios_base::sync_with_stdio(false);
+            cin.tie(NULL);
+            int minSoFar = prices[0], maxProfit = 0, currProfit = 0;
+
+            for (auto &v: prices)
+            {
+                minSoFar = min(minSoFar, v);
+                currProfit = v - minSoFar;
+                maxProfit = max(currProfit, maxProfit);
+            }
+        if(maxProfit<0 or maxProfit==0)
+        {
+            return -1;
+        }
+            return maxProfit;
+    }
+};
