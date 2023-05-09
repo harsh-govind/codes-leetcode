0fdb479e2f6162e1e7abd96de0e62cafa0f8f7ba
Harsh Govind <harsh5092001@gmail.com>
2023-05-09 09:59:09 +0530

    Time: 4 ms (60.30%), Space: 12 MB (73.40%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -3,31 +3,6 @@ class Solution
     public:
         void nextPermutation(vector<int> &nums)
         {
-            int beforeTop = -1, n = nums.size();
-            for (int i = n - 1; i > 0; i--)
-            {
-                if (nums[i - 1] < nums[i])
-                {
-                    beforeTop = i-1;
-                    break;
-                }
-            }
-            cout<<beforeTop<<endl;
-            if (beforeTop == -1)
-            {
-                reverse(nums.begin(), nums.end());
-                return;
-            }
-
-            for (int i = n - 1; i > beforeTop; i--)
-            {
-                if (nums[i] > nums[beforeTop])
-                {
-                    swap(nums[beforeTop], nums[i]);
-                    break;
-                }
-            }
-
-            reverse(nums.begin() + beforeTop+1, nums.end());
+            next_permutation(nums.begin(), nums.end());
         }
 };
