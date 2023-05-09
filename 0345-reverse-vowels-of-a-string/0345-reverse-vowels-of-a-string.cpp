d4f9922464dfbd8ca022cb24a5baaab04c0ae140
Harsh Govind <harsh5092001@gmail.com>
2023-05-09 22:21:07 +0530

    Time: 4 ms (80.50%), Space: 7.8 MB (28.05%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,41 +1,59 @@
 class Solution
 {
     public:
-        string reverseVowels(string s)
+        bool c(char a)
         {
-            string t = "";
-            for (int i = 0; i < s.size(); i++)
-            {
-                if (s[i] == 'a'
-                    or s[i] == 'e'
-                    or s[i] == 'i'
-                    or s[i] == 'o'
-                    or s[i] == 'u' or s[i] == 'A'
-                    or s[i] == 'E'
-                    or s[i] == 'I'
-                    or s[i] == 'O'
-                    or s[i] == 'U')
-                {
-                    t += s[i];
-                }
-            }
-            int p = 0;
-            for (int i = s.size() - 1; i >= 0; i--)
-            {
-                if (s[i] == 'a'
-                    or s[i] == 'e'
-                    or s[i] == 'i'
-                    or s[i] == 'o'
-                    or s[i] == 'u' or s[i] == 'A'
-                    or s[i] == 'E'
-                    or s[i] == 'I'
-                    or s[i] == 'O'
-                    or s[i] == 'U')
-                {
-                    s[i] = t[p++];
-                }
-            }
-
-            return s;
+            return (a == 'a'
+                or a == 'e'
+                or a == 'i'
+                or a == 'o'
+                or a == 'u'
+                or a == 'A'
+                or a == 'E'
+                or a == 'I'
+                or a == 'O'
+                or a == 'U');
         }
+    string reverseVowels(string a)
+    {
+       	// string t = "";
+       	// for (int i = 0; i < s.size(); i++)
+       	// {
+       	//     if (c(a[i]))
+       	//     {
+       	//         t += s[i];
+       	//     }
+       	// }
+       	// int p = 0;
+       	// for (int i = s.size() - 1; i >= 0; i--)
+       	// {
+       	//     if (c(a[i]))
+       	//     {
+       	//         s[i] = t[p++];
+       	//     }
+       	// }
+
+        int i = 0, j = a.size() - 1;
+        while (i < j)
+        {
+            if (c(a[i]))
+            {
+                if (c(a[j]))
+                {
+                    swap(a[i], a[j]);
+                    i++;
+                    j--;
+                }
+                else
+                {
+                    j--;
+                }
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return a;
+    }
 };
