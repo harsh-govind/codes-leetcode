d2c1ed7349cb9755c32e1a35dc801be11dadf1fc
Harsh Govind <harsh5092001@gmail.com>
2023-05-09 12:33:31 +0530

    Time: 11 ms (93.57%), Space: 19.7 MB (97.30%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,26 +1,61 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
+//         o(n) o(n)
+//         int n=height.size();
+//         vector<int> left(n), right(n);
+//         left[0]=height[0];
+//         right[n-1]=height[n-1];
+        
+//         for(int i=1;i<n;i++)
+//         {
+//             left[i]=max(left[i-1], height[i]);
+//         }
+//         for(int i=n-2; i>=0; i--)
+//         {
+//             right[i]=max(right[i+1], height[i]);
+//         }
+        
+//         int ans=0;
+//         for(int i=0; i<n; i++)
+//         {
+//             ans+=min(left[i], right[i])-height[i];
+//         }
+        
+//         return ans;
+        
+//         o(n) o(1);
+        
         int n=height.size();
-        vector<int> left(n), right(n);
-        left[0]=height[0];
-        right[n-1]=height[n-1];
+        int maxLeft=height[0], maxRight=height[n-1], left=1, right=n-2, ans=0;
         
-        for(int i=1;i<n;i++)
+        while(left<=right)
         {
-            left[i]=max(left[i-1], height[i]);
+            if(maxLeft<=maxRight)
+            {
+                if(height[left]>maxLeft)
+                {
+                    maxLeft=height[left];
+                }
+                else
+                {
+                    ans+=maxLeft-height[left];
+                }
+                left++;
+            }
+            else
+            {
+                if(height[right]>maxRight)
+                {
+                    maxRight=height[right];
+                }
+                else
+                {
+                    ans+=maxRight-height[right];
+                }
+                right--;
+            }
         }
-        for(int i=n-2; i>=0; i--)
-        {
-            right[i]=max(right[i+1], height[i]);
-        }
-        
-        int ans=0;
-        for(int i=0; i<n; i++)
-        {
-            ans+=min(left[i], right[i])-height[i];
-        }
-        
         return ans;
     }
 };
