c1afcb46f55f61f194fee30f33045dc2a32a80a6
Harsh Govind <harsh5092001@gmail.com>
2023-05-10 10:58:37 +0530

    Added solution - LeetHub

--- First and last occurrences of x - GFG/first-and-last-occurrences-of-x.cpp
+++ First and last occurrences of x - GFG/first-and-last-occurrences-of-x.cpp
@@ -5,17 +5,40 @@ using namespace std;
 // } Driver Code Ends
 vector<int> find(int arr[], int n , int x )
 {
-   vector<int> ans;
-   auto it=lower_bound(arr, arr+n, x),  it2=upper_bound(arr, arr+n, x);
-   if(it==(arr+n) or *it!=x)
-   {
-        ans.push_back(-1);
-        ans.push_back(-1);
-        return ans;
-   }
-   ans.push_back(it-arr);
-   ans.push_back(it2-arr-1);
-   return ans;
+   vector<int> fans;
+   int low = 0, high = n - 1, ans = -1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] == x)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else if (arr[mid] > x)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+   fans.push_back(ans);
+   low = 0;
+   high = n - 1;
+   ans = -1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] == x)
+        {
+            ans = mid;
+            low = mid + 1;
+        }
+        else if (arr[mid] > x)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+   fans.push_back(ans);
+   return fans;
 }
 
 //{ Driver Code Starts.
