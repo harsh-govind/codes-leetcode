dcbdfbf7eca3d85994a3c6d1abc6482bc78824f0
Harsh Govind <harsh5092001@gmail.com>
2023-05-12 11:28:30 +0530

    Time: 397 ms (28.09%), Space: 115.1 MB (93.26%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -1,22 +1,25 @@
-class Solution {
-public:
-    long long mostPoints(vector<vector<int>>& questions) {
-    int n = questions.size();
-    vector<long long> dp(n, 0);
-    dp[n - 1] = questions[n - 1][0];
-    
-    for (int i = n - 2; i >= 0; --i) {
-        dp[i] = questions[i][0];
-        int skip = questions[i][1];
-        if (i + skip + 1 < n) {
-            dp[i] += dp[i + skip + 1];
-        }
-        
-        // dp[i] = max(solve it, skip it)
-        dp[i] = max(dp[i], dp[i + 1]);
-    }
-    
-    return dp[0];
-}
+class Solution
+{
+    public:
+        long long mostPoints(vector<vector < int>> &questions)
+        {
+            int n = questions.size();
+            vector < long long > dp(n, 0);
+            dp[n - 1] = questions[n - 1][0];
 
+            for (int i = n - 2; i >= 0; --i)
+            {
+                dp[i] = questions[i][0];
+                int skip = questions[i][1];
+                if (i + skip + 1 < n)
+                {
+                    dp[i] += dp[i + skip + 1];
+                }
+
+               	// dp[i] = max(solve it, skip it)
+                dp[i] = max(dp[i], dp[i + 1]);
+            }
+
+            return dp[0];
+        }
 };
