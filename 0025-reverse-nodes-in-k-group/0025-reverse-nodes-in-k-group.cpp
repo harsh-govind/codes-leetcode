be477a53c3da7354f025bf6955a420d86dbf1662
Harsh Govind <harsh5092001@gmail.com>
2023-05-18 14:00:50 +0530

    Time: 16 ms (64.91%), Space: 11.4 MB (68.07%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -1,13 +1,59 @@
 /**
  *Definition for singly-linked list.
  *struct ListNode {
- *  int val;
- *  ListNode * next;
- *  ListNode() : val(0), next(nullptr) {}
- *  ListNode(int x) : val(x), next(nullptr) {}
- *  ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *    int val;
+ *    ListNode * next;
+ *    ListNode() : val(0), next(nullptr) {}
+ *    ListNode(int x) : val(x), next(nullptr) {}
+ *    ListNode(int x, ListNode *next) : val(x), next(next) {}
  *};
  */
+class Solution
+{
+    public:
+        int length(ListNode *head)
+        {
+            int ans = 0;
+            while (head)
+            {
+                ans++;
+                head = head->next;
+            }
+            return ans;
+        }
+    ListNode* reverseg(int len, ListNode *head, int k)
+    {
+        if (len < k)
+        {
+            return head;
+        }
+
+        ListNode *p = head, *q = nullptr, *r = nullptr;
+        int count = 0;
+
+        while (count < k and p)
+        {
+            r = q;
+            q = p;
+            p = p->next;
+            q->next=r;
+            count++;
+        }
+        if (p)
+        {
+            head->next = reverseg(len - k, p, k);
+        }
+        return q;
+    }
+    ListNode* reverseKGroup(ListNode *head, int k)
+    {
+        int len = length(head);
+        return reverseg(len, head, k);
+    }
+};
+
+/*
+// SPACE
 class Solution
 {
     public:
@@ -31,7 +77,8 @@ class Solution
                 temp[i]->next=temp[i+1];
             }
             temp[temp.size()-1]->next=nullptr;
-            
+
             return temp[0];
         }
 };
+*/
