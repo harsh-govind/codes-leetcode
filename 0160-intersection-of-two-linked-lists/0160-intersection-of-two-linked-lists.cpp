027f835017042eae1acd2c826dbffb6a86d56bbe
Harsh Govind <harsh5092001@gmail.com>
2023-05-19 18:01:01 +0530

    Time: 48 ms (65.97%), Space: 14.7 MB (35.54%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -1,22 +1,45 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *   int val;
+ *   ListNode * next;
+ *   ListNode(int x) : val(x), next(NULL) {}
+ *};
  */
+class Solution
+{
+    public:
+        ListNode* getIntersectionNode(ListNode *a, ListNode *b)
+        {
+            if (!a or!b)
+            {
+                return nullptr;
+            }
+
+            ListNode *p = a, *q = b;
+
+            while (p != q)
+            {
+                p = p == nullptr ? b : p->next;
+                q = q == nullptr ? a : q->next;
+            }
+            return p;
+        }
+};
+
+/*
+//space
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_set<ListNode *> s;
-        ListNode *p=headA;
+    ListNode* getIntersectionNode(ListNode *headA, ListNode *headB) {
+        unordered_set<ListNode*> s;
+        ListNode *p =headA;
         while(p)
         {
             s.insert(p);
             p=p->next;
         }
-        
+
         p=headB;
         while(p)
         {
@@ -27,7 +50,8 @@ public:
             s.insert(p);
             p=p->next;
         }
-        
+
         return nullptr;
     }
 };
+*/
