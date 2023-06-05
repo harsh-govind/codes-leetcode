817aa8ce109eae2b49df8a2478401d5ed481a95e
Harsh Govind <harsh5092001@gmail.com>
2023-06-05 17:56:05 +0530

    Time: 70 ms (75.90%), Space: 13.6 MB (36.12%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,39 +1,44 @@
-class Solution {
-public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        set<vector<int>> ans;
-        int n=nums.size();
-        long long sum=0;
-        sort(nums.begin(), nums.end());
-        for(int i=0; i<n-3; i++)
+class Solution
+{
+    public:
+        vector<vector < int>> fourSum(vector<int> &nums, int target)
         {
-            if(i>0 and nums[i]==nums[i-1]) continue;
-            for(int j=i+1; j<n-2; j++)
+            set<vector < int>> ans;
+            int n = nums.size();
+            long long sum = 0;
+            sort(nums.begin(), nums.end());
+            for (int i = 0; i < n - 3; i++)
             {
-                if(j>i+1 and nums[j]==nums[j-1]) continue;
-                int left=j+1, right=n-1;
-                while(left<right)
+                if (i > 0 and nums[i] == nums[i - 1]) continue;
+                for (int j = i + 1; j < n - 2; j++)
                 {
-                    sum=nums[i]+nums[j];
-                    sum+=nums[left];
-                    sum+=nums[right];
-                    if(sum==target)
+                    if (j > i + 1 and nums[j] == nums[j - 1]) continue;
+                    int left = j + 1, right = n - 1;
+                    while (left < right)
                     {
-                        ans.insert({nums[i], nums[j], nums[left], nums[right]});
-                        left++;
-                    }
-                    else if(sum>target)
-                    {
-                        right--;
-                    }
-                    else
-                    {
-                        left++;
+                        sum = nums[i] + nums[j];
+                        sum += nums[left];
+                        sum += nums[right];
+                        if (sum == target)
+                        {
+                            ans.insert({ nums[i],
+                                nums[j],
+                                nums[left],
+                                nums[right] });
+                            left++;
+                        }
+                        else if (sum > target)
+                        {
+                            right--;
+                        }
+                        else
+                        {
+                            left++;
+                        }
                     }
                 }
             }
+            vector<vector < int>> finalAns(ans.begin(), ans.end());
+            return finalAns;
         }
-        vector<vector<int>> finalAns(ans.begin(), ans.end());
-        return finalAns;
-    }
 };
