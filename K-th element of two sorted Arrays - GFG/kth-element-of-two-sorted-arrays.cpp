50ebdb92757f4c4a86c252650a1042950802a084
Harsh Govind <harsh5092001@gmail.com>
2023-06-07 18:52:11 +0530

    Added solution - LeetHub

--- K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
@@ -8,48 +8,38 @@ class Solution{
     public:
     int kthElement(int arr1[], int arr2[], int m, int n, int k)
     {
-            int i = 0, j = 0, t = 0;
-    while (i < m and j < n and t < k)
-    {
-        if (arr1[i] < arr2[j])
+        if(m>n)
         {
-            t++;
-            if (t == k)
+            return kthElement(arr2, arr1, n, m, k);
+        }
+        
+        int low=max(0, k-n), high=min(k, m);
+        
+        while(low<=high)
+        {
+            int cut1=(low+high)/2;
+            int cut2=(k-cut1);
+            
+            int l1=cut1==0?INT_MIN:arr1[cut1-1];
+            int l2=cut2==0?INT_MIN:arr2[cut2-1];
+            
+            int r1=cut1==m?INT_MAX:arr1[cut1];
+            int r2=cut2==n?INT_MAX:arr2[cut2];
+            
+            if(l1<=r2 and l2<=r1)
             {
-                return arr1[i];
+                return max(l1, l2);
             }
-            i++;
-        }
-        else
-        {
-            t++;
-            if (t == k)
+            else if(l1>r2)
             {
-                return arr2[j];
+                high=cut1-1;
             }
-            j++;
+            else
+            {
+                low=cut1+1;
+            }
         }
-    }
-    // Handle remaining elements in arr1 or arr2
-    while (i < m and t < k)
-    {
-        t++;
-        if (t == k)
-        {
-            return arr1[i];
-        }
-        i++;
-    }
-    while (j < n and t < k)
-    {
-        t++;
-        if (t == k)
-        {
-            return arr2[j];
-        }
-        j++;
-    }
-    return -1; // If k is out of range
+        return -1;
     }
 };
 
