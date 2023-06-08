4c636170cadf56abe9d5ec22e74cf29ffb959a07
Harsh Govind <harsh5092001@gmail.com>
2023-06-08 23:06:02 +0530

    Time: 632 ms (25.57%), Space: 67.1 MB (23.00%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,28 +1,30 @@
-class Solution {
-public:
-    int longestConsecutive(vector<int>& nums) {
-        if(nums.size()==0 or nums.size()==1) return nums.size();
-        unordered_map<int, int> m;
-        int curr=0, maxx=0;
-        for(auto &val:nums)
+class Solution
+{
+    public:
+        int longestConsecutive(vector<int> &nums)
         {
-            m[val]++;
-        }
-        int ans=INT_MIN;
-        for(auto &val:nums)
-        {
-            if(m.find(val-1)==m.end())
+            if (nums.size() == 0 or nums.size() == 1) return nums.size();
+            unordered_set<int> m;
+            int curr = 0, maxx = 0;
+            for (auto &val: nums)
             {
-                int currVal=val, temp=1;
-                while(m.find(currVal+1)!=m.end())
-                {
-                    temp++;
-                    currVal+=1;
-                }
-                
-                ans=max(temp, ans);
+                m.insert(val);
             }
+            int ans = INT_MIN;
+            for (auto &val: nums)
+            {
+                if (m.find(val - 1) == m.end())
+                {
+                    int currVal = val, temp = 1;
+                    while (m.find(currVal + 1) != m.end())
+                    {
+                        temp++;
+                        currVal += 1;
+                    }
+
+                    ans = max(temp, ans);
+                }
+            }
+            return ans;
         }
-        return ans;
-    }
 };
