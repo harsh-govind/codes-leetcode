a7e9cd318527cd4d02e1f18fa9edfda3d7b168cd
Harsh Govind <harsh5092001@gmail.com>
2023-06-10 13:43:15 +0530

    Time: 0 ms (100.00%), Space: 10.5 MB (44.92%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void solve(vector<int> &nums,vector<vector<int>> &ans, vector<int> current, int i)
+    void solve(vector<int> &nums,vector<vector<int>> &ans, vector<int> &current, int i)
     {
         if(i==nums.size())
         {
@@ -14,6 +14,9 @@ public:
         //include
         current.push_back(nums[i]);
         solve(nums, ans, current, i+1);
+        
+        //uncomment the line below if you want to send current as reference
+        current.pop_back();
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
