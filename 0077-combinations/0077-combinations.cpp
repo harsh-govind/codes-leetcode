608ad3ab24a36407c8a5011aa0b25b6333e76ecb
Harsh Govind <harsh5092001@gmail.com>
2023-06-11 13:41:18 +0530

    Time: 37 ms (34.62%), Space: 17.9 MB (32.69%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+    public:
+        void solve(int i, int k, vector<int> &current, vector<vector< int>> &ans, int n)
+        {
+            if (current.size() == k)
+            {
+                ans.push_back(current);
+                return;
+            }
+
+           	//include
+            if (i <= n)
+            {
+                current.push_back(i);
+                solve(i + 1, k, current, ans, n);
+                current.pop_back();
+
+               	//exclude
+                solve(i + 1, k, current, ans, n);
+            }
+        }
+    vector<vector < int>> combine(int n, int k)
+    {
+        vector<int> current;
+        vector<vector < int>> ans;
+        solve(1, k, current, ans, n);
+        return ans;
+    }
+};
