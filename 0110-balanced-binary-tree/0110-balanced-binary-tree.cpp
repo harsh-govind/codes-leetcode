5fca15b826ab588a4e7a5917a296c731bc3dec84
Harsh Govind <harsh5092001@gmail.com>
2023-06-20 17:51:08 +0530

    Time: 19 ms (29.64%), Space: 22.1 MB (7.04%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -1,42 +1,40 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *   int val;
+ *   TreeNode * left;
+ *   TreeNode * right;
+ *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
 {
     public:
-        int height(TreeNode *root)
+        pair<bool, int> solve(TreeNode *root)
         {
             if (root == nullptr)
             {
-                return 0;
+                pair<bool, int> p = make_pair(true, 0);
+                return p;
             }
 
-            int left = height(root->left);
-            int right = height(root->right);
+            pair<bool, int> left = solve(root->left);
+            pair<bool, int> right = solve(root->right);
 
-            int ans = max(left, right) + 1;
+            bool op1 = left.first;
+            bool op2 = right.first;
+            bool op3 = abs(left.second - right.second) < 2;
+
+            pair<bool, int> ans;
+            ans.first = op1 and op2 and op3;
+            ans.second = max(left.second, right.second) + 1;
 
             return ans;
         }
     bool isBalanced(TreeNode *root)
     {
-        if (root == nullptr)
-        {
-            return true;
-        }
-
-        bool op1 = isBalanced(root->left);
-        bool op2 = isBalanced(root->right);
-        bool op3 = abs(height(root->left) - height(root->right)) < 2;
-
-        return op1 and op2 and op3;
+        return solve(root).first;
     }
 };
