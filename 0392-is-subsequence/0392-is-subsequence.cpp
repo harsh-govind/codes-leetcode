709d583e367d0cceeb357519715a74c07af628a0
Harsh Govind <harsh5092001@gmail.com>
2023-06-25 20:39:02 +0530

    Time: 4 ms (46.29%), Space: 6.5 MB (25.68%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+    public:
+        bool isSubsequence(string s, string t)
+        {
+            if (s.size() > t.size())
+                return false;
+
+            int i = 0, j = 0;
+            while (j < t.size())
+            {
+                if (t[j] == s[i])
+                {
+                    i++;
+                }
+                j++;
+
+                if (i == s.size()) return true;
+            }
+            cout<<i<<" "<<j<<endl;
+            if (i == s.size()) return true;
+            return false;
+        }
+};
