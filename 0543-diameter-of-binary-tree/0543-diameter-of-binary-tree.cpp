83e56b9ce1c413ac502e2436e1888f9efe88d37a
Harsh Govind <harsh5092001@gmail.com>
2023-06-26 19:19:16 +0530

    Time: 15 ms (43.64%), Space: 20.8 MB (5.05%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -1,34 +1,39 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *  int val;
- *  TreeNode * left;
- *  TreeNode * right;
- *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *int val;
+ *TreeNode * left;
+ *TreeNode * right;
+ *TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
 {
     public:
-        int height(TreeNode *root)
+    pair<int, int> solve(TreeNode *root)
+    {
+        if (!root)
         {
-            if (!root) return 0;
-
-            int left = height(root->left);
-            int right = height(root->right);
-
-            return max(left, right)+1;
+            return { 0,
+                0 };
         }
+
+        pair<int, int> left = solve(root->left);
+        pair<int, int> right = solve(root->right);
+
+        int op1 = left.first;
+        int op2 = right.first;
+        int op3 = left.second + right.second;
+
+        pair<int, int> ans;
+        ans.first = max(op1, max(op2, op3));
+        ans.second = max(left.second, right.second) + 1;
+        return ans;
+    }
     int diameterOfBinaryTree(TreeNode *root)
     {
-        if (!root) return 0;
-
-        int left = diameterOfBinaryTree(root->left);
-        int right = diameterOfBinaryTree(root->right);
-        int current = height(root->right) + height(root->left);
-
-        return max(left, max(right, current));
+        return solve(root).first;
     }
 };
