e2f148b2f39d84e41972932c057822bb9dfab6bb
Harsh Govind <harsh5092001@gmail.com>
2023-06-26 19:14:54 +0530

    Time: 37 ms (5.31%), Space: 20.2 MB (81.75%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -1,42 +1,34 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *  int val;
+ *  TreeNode * left;
+ *  TreeNode * right;
+ *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
 {
     public:
-    pair<int, int> diameter(TreeNode *root)
-    {
-        if(root==nullptr)
+        int height(TreeNode *root)
         {
-            pair<int, int> p=make_pair(0, 0);
-            return p;
+            if (!root) return 0;
+
+            int left = height(root->left);
+            int right = height(root->right);
+
+            return max(left, right)+1;
         }
-        
-        pair<int, int> left=diameter(root->left);
-        pair<int, int> right=diameter(root->right);
-        
-        //diameter- first
-        //height- second
-        int op1=left.first;
-        int op2=right.first;
-        int op3=left.second+right.second;
-        
-        pair<int, int> ans;
-        ans.first=max(op1, max(op2, op3));
-        ans.second=max(left.second, right.second)+1;
-        
-        return ans;     
-    }
     int diameterOfBinaryTree(TreeNode *root)
     {
-        return diameter(root).first;
+        if (!root) return 0;
+
+        int left = diameterOfBinaryTree(root->left);
+        int right = diameterOfBinaryTree(root->right);
+        int current = height(root->right) + height(root->left);
+
+        return max(left, max(right, current));
     }
 };
