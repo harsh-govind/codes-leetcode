8a984187815b3ce99f5359efc1cb8962a2486948
Harsh Govind <harsh5092001@gmail.com>
2023-06-26 14:22:03 +0530

    Time: 3 ms (51.68%), Space: 5.9 MB (95.67%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -6,8 +6,7 @@ class Solution
             int res = 0;
             for (int i = 0; i < s.size(); i++)
             {
-                res = res * 26;
-                res += (s[i] - 'A' + 1);
+                res = res *26 + (s[i] - 'A' + 1);
             }
             return res;
         }
