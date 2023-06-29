bd4dec39e37d786aceac29ea3e92fa41c61bd363
Harsh Govind <harsh5092001@gmail.com>
2023-06-29 14:51:56 +0530

    Added solution - LeetHub

---
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+
+	public:
+	int perfectSum(int arr[], int n, int sum)
+	{
+        long long m = 1e9+7;
+        vector<vector<int>> dp(n+1, vector<int>(sum+1));
+        for(int i=0; i<sum+1; i++)
+        {
+            dp[0][i]=0;
+        }
+        for(int i=0; i<n+1; i++)
+        {
+            dp[i][0]=1;
+        }
+        
+        for(int i=1; i<n+1; i++)
+        {
+            for(int j=0; j<sum+1; j++)
+            {
+                if(arr[i-1]<=j)
+                {
+                    dp[i][j]= (dp[i-1][j-arr[i-1]] + dp[i-1][j])%m;
+                }
+                else
+                {
+                    dp[i][j] = dp[i-1][j]%m;
+                }
+            }
+        }
+        return dp[n][sum];
+	}
+
+	  
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, sum;
+
+        cin >> n >> sum;
+
+        int a[n];
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+       
+
+	    Solution ob;
+	    cout << ob.perfectSum(a, n, sum) << "\n";
+	     
+    }
+    return 0;
+}
+
+// } Driver Code Ends
