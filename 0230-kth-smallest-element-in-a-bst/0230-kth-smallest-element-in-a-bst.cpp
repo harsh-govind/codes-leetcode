60cdbe7602d2ae707ee5a29bdaef21432a29014e
Harsh Govind <harsh5092001@gmail.com>
2023-07-01 19:02:26 +0530

    Time: 23 ms (41.79%), Space: 24 MB (97.32%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -1,30 +1,57 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *  int val;
+ *  TreeNode * left;
+ *  TreeNode * right;
+ *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
 {
     public:
-        int kthSmallest(TreeNode *root, int &k)
+        TreeNode* predecessor(TreeNode *current)
         {
-            if (!root) return -1;
-
-            int left = kthSmallest(root->left, k);
-
-            if (left != -1)
-                return left;
-
-            k--;
-            if (k == 0)
-                return root->val;
-
-            return kthSmallest(root->right, k);
+            TreeNode *ans = current->left;
+            while (ans->right and ans->right != current)
+            {
+                ans = ans->right;
+            }
+            return ans;
         }
+    int kthSmallest(TreeNode *root, int &k)
+    {
+        int i=0;
+        int ans=-1;
+        TreeNode *curr = root;
+        while (curr)
+        {
+            if (!curr->left)
+            {
+                i++;
+                if(i==k) ans=curr->val;
+                curr = curr->right;
+            }
+            else
+            {
+                TreeNode *pred = predecessor(curr);
+
+                if (!pred->right)
+                {
+                    pred->right = curr;
+                    curr = curr->left;
+                }
+                else
+                {
+                    pred->right = nullptr;
+                    i++;
+                    if(i==k) ans=curr->val;
+                    curr = curr->right;
+                }
+            }
+        }
+        return ans;
+    }
 };
