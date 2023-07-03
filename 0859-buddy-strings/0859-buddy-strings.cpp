f4ce4424c87d7f5491842e03528615f6ad268d3c
Harsh Govind <harsh5092001@gmail.com>
2023-07-03 18:57:08 +0530

    Time: 7 ms (35.87%), Space: 7 MB (19.51%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <string>
+
+class Solution
+{
+public:
+    bool buddyStrings(string s, string goal)
+    {
+        int count = 0;
+        vector<int> idx;
+        if (s.size() != goal.size())
+            return false;
+
+        if (s == goal)
+        {
+            // Check if s contains at least one repeated character
+            vector<int> freq(26, 0);
+            for (char c : s)
+            {
+                freq[c - 'a']++;
+                if (freq[c - 'a'] > 1)
+                    return true;
+            }
+            return false;
+        }
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] != goal[i])
+            {
+                count++;
+                if (count > 2)
+                    return false;
+                idx.push_back(i);
+            }
+        }
+
+        if (count != 2)
+            return false;
+
+        swap(s[idx[0]], s[idx[1]]);
+
+        return s == goal;
+    }
+};
