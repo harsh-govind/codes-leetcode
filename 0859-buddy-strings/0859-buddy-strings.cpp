85ecddb9a782daed72d0818eb2178d8e095dc3af
Harsh Govind <harsh5092001@gmail.com>
2023-07-03 18:58:33 +0530

    Time: 7 ms (35.87%), Space: 6.8 MB (85.37%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.cpp
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -3,43 +3,41 @@
 
 class Solution
 {
-public:
-    bool buddyStrings(string s, string goal)
-    {
-        int count = 0;
-        vector<int> idx;
-        if (s.size() != goal.size())
-            return false;
-
-        if (s == goal)
+    public:
+        bool buddyStrings(string s, string goal)
         {
-            // Check if s contains at least one repeated character
-            vector<int> freq(26, 0);
-            for (char c : s)
+            int count = 0;
+            vector<int> idx;
+            if (s.size() != goal.size())
+                return false;
+            if (s == goal)
             {
-                freq[c - 'a']++;
-                if (freq[c - 'a'] > 1)
-                    return true;
+                vector<int> freq(26, 0);
+                for (char &c: s)
+                {
+                    freq[c - 'a']++;
+                    if (freq[c - 'a'] > 1)
+                        return true;
+                }
+                return false;
             }
-            return false;
-        }
 
-        for (int i = 0; i < s.size(); i++)
-        {
-            if (s[i] != goal[i])
+            for (int i = 0; i < s.size(); i++)
             {
-                count++;
-                if (count > 2)
-                    return false;
-                idx.push_back(i);
+                if (s[i] != goal[i])
+                {
+                    count++;
+                    if (count > 2)
+                        return false;
+                    idx.push_back(i);
+                }
             }
+
+            if (count != 2)
+                return false;
+
+            swap(s[idx[0]], s[idx[1]]);
+
+            return s == goal;
         }
-
-        if (count != 2)
-            return false;
-
-        swap(s[idx[0]], s[idx[1]]);
-
-        return s == goal;
-    }
 };
