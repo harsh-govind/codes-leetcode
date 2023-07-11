6573b09d560d2c4893facef45fd223ac1d095a45
Harsh Govind <harsh5092001@gmail.com>
2023-07-11 16:27:07 +0530

    Time: 51 ms (19.96%), Space: 33.4 MB (37.20%) - LeetHub

--- 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.cpp
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.cpp
@@ -1,50 +1,26 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    void findInorder(TreeNode *root, vector<int> &inorder)
+class Solution
+{
+    public:
+    int sum=0;
+    TreeNode* convertBST(TreeNode *root)
     {
-        if(!root) return;
-        
-        findInorder(root->left, inorder);
-        inorder.push_back(root->val);
-        findInorder(root->right, inorder);
-    }
-    void solve(TreeNode *root, unordered_map<int, int> &m, vector<int> &sum, int n)
-    {
-        if(!root) return;
-        
-        root->val+=sum[n-1]-sum[m[root->val]];
-        
-        solve(root->left, m, sum, n);
-        solve(root->right, m, sum, n);
-    }
-    TreeNode* convertBST(TreeNode* root) {
-        if(!root) return nullptr;
-        vector<int> inorder;
-        findInorder(root, inorder);
-        int n=inorder.size();
-        vector<int> sum(n);
-        unordered_map<int, int> m;
-        sum[0]=inorder[0];
-        m[inorder[0]]=0;
-        for(int i=1; i<n; i++)
-        {
-            sum[i]=sum[i-1]+inorder[i];
-            m[inorder[i]]=i;
-        }
-        
-        solve(root, m, sum, n);
+        if (!root) return nullptr;
+        convertBST(root->right);
+        sum += root->val;
+        root->val = sum;
+        convertBST(root->left);
+
         return root;
-        
     }
 };
