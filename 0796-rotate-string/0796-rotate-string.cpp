a9ab0d3e5b6da0150bc7b449be24205395e86f03
Harsh Govind <harsh5092001@gmail.com>
2023-07-13 02:27:53 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (60.94%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,25 +1,23 @@
-class Solution {
-public:
-    string rotate(string &b)
+class Solution
+{
+    public:
+
+    bool rotateString(string a, string b)
     {
-        char temp=b[0];
-        int n=b.size();
-        for(int i=0; i<n-1; i++)
+        if (a == b or a.size() == 0 or b.size() == 0) return true;
+        if (a.size() != b.size()) return false;
+
+        int n = a.size();
+        string temp=a+a;
+
+        for (int i = 0; i < n; i++)
         {
-            b[i]=b[i+1];
-        }
-        b[n-1]=temp;
-        return b;
-    }
-    bool rotateString(string a, string b) {
-        if(a==b or a.size()==0 or b.size()==0) return true;
-        if(a.size()!=b.size()) return false;
-        
-        int n=a.size();
-        
-        for(int i=0; i<n; i++)
-        {
-            if(a==rotate(b)) return true;       
+            int j;
+            for(j=0; j<n; j++)
+            {
+                if(b[j]!=temp[i+j]) break;
+            }
+            if(j==n) return true;
         }
         return false;
     }
