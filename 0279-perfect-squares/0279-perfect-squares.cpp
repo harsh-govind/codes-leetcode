17ec082ae3af917b35d019f4dec55497114cd5ee
Harsh Govind <harsh5092001@gmail.com>
2023-07-16 20:25:53 +0530

    Time: 337 ms (28.58%), Space: 9.1 MB (70.21%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -18,7 +18,18 @@ class Solution
 
     int numSquares(int n)
     {
-        vector<int> dp(n + 1, -1);
-        return solve(n, dp);
+        vector<int> dp(n + 1, 999999);
+        dp[0]=0;
+        
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j*j<=n; j++)
+            {
+                int temp=j*j;
+                if(i>=temp)
+                    dp[i]=min(dp[i], dp[i-temp]+1);
+            }
+        }
+        return dp[n];
     }
 };
