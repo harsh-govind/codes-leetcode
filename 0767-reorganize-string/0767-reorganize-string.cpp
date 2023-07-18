d01e2f59195a499ac765677da2d41492051c11da
Harsh Govind <harsh5092001@gmail.com>
2023-07-18 13:47:38 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (95.82%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+    public:
+        string reorganizeString(string s)
+        {
+            unordered_map<char, int> m;
+            priority_queue<pair<int, char>> p;
+            for (auto &val: s)
+            {
+                m[val]++;
+            }
+            for (auto &val: m)
+            {
+                p.push({ val.second,
+                    val.first });
+            }
+            string ans = "";
+            pair<int, char> block = { -1,'#'};
+            while (p.size() > 0)
+            {
+                auto temp = p.top();
+                p.pop();
+                ans += temp.second;
+                temp.first--;
+
+                if (block.first > 0)
+                {
+                    p.push(block);
+                }
+                block = temp;
+            }
+            return ans.size() == s.size() ? ans : "";
+        }
+};
