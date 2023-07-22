0521af7130cb9cd8ba9460dc99f81b0e20f52962
Harsh Govind <harsh5092001@gmail.com>
2023-07-22 14:31:17 +0530

    Added solution - LeetHub

---
+++ Prerequisite Tasks - GFG/prerequisite-tasks.cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+	bool isPossible(int v,int P, vector<pair<int, int> >& prerequisites) {
+	    // Code here
+	    vector<int> adj[v];
+	    for(auto &i:prerequisites)
+	    {
+	        adj[i.first].push_back(i.second);
+	    }
+	    
+	    int indegree[v]={0};
+	    
+	    for(int i=0; i<v; i++)
+	    {
+	        for(auto &it:adj[i])
+	        {
+	            indegree[it]++;
+	        }
+	    }
+	    
+	    queue<int> q;
+	    for(int i=0; i<v; i++)
+	    {
+	        if(indegree[i]==0)
+	        {
+	            q.push(i);
+	        }
+	    }
+	    vector<int> topo;
+	    while(!q.empty())
+	    {
+	        int front=q.front();
+	        q.pop();
+	        topo.push_back(front);
+	        
+	        for(auto &it:adj[front])
+	        {
+	            indegree[it]--;
+	            if(indegree[it]==0)
+	            {
+	                q.push(it);
+	            }
+	        }
+	    }
+	    
+	    return topo.size()==v;
+	    
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+    	int N, P;
+        vector<pair<int, int> > prerequisites;
+        cin >> N;
+        cin >> P;
+        for (int i = 0; i < P; ++i) {
+            int x, y;
+            cin >> x >> y;
+            prerequisites.push_back(make_pair(x, y));
+        }
+        // string s;
+        // cin>>s;
+        Solution ob;
+        if (ob.isPossible(N,P, prerequisites))
+            cout << "Yes";
+        else
+            cout << "No";
+        cout << endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
