735c6e7168d38b9edb68bb21c9a4610719878ab0
Harsh Govind <harsh5092001@gmail.com>
2023-07-25 13:09:50 +0530

    Time: 34 ms (81.57%), Space: 38.2 MB (88.49%) - LeetHub

---
+++ 2788-split-strings-by-separator/2788-split-strings-by-separator.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> splitWordsBySeparator(vector<string>& words, char separator) {
+        vector<string> ans;
+        for(auto &word:words)
+        {
+            string current="";
+            for(auto &ch:word)
+            {
+                if(ch==separator)
+                {
+                    if(current!="")
+                    ans.push_back(current);
+                    current="";
+                }
+                else
+                {
+                    current+=ch;
+                }
+            }
+            if(current.size()>0 and current!="")
+            {
+                ans.push_back(current);
+            }
+        }
+        return ans;
+    }
+};
