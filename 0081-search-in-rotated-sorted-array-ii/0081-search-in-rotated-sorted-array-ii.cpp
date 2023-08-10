855631721a2f7fd85792694d684c358772d9f663
Harsh Govind <harsh5092001@gmail.com>
2023-08-10 22:23:46 +0530

    Time: 3 ms (91.34%), Space: 14.1 MB (18.44%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,8 +1,47 @@
 class Solution
 {
     public:
-        bool search(vector<int> &nums, int x)
+        bool solve(vector<int> &nums, int l, int h, int x)
         {
-            return find(nums.begin(), nums.end(), x) != nums.end();
+            while (l <= h)
+            {
+                int m = (l + h) / 2;
+                if (nums[m] == x)
+                {
+                    return true;
+                }
+                else if (nums[l] < nums[m])
+                {
+                    if (x >= nums[l] and x <= nums[m])
+                    {
+                        h = m - 1;
+                    }
+                    else
+                    {
+                        l = m + 1;
+                    }
+                }
+                else if (nums[m] < nums[l])
+                {
+                    if (x >= nums[m] and x <= nums[h])
+                    {
+                        l = m + 1;
+                    }
+                    else
+                    {
+                        h = m - 1;
+                    }
+                }
+                else
+                {
+                    l++;
+                }
+            }
+
+            return false;
         }
+    bool search(vector<int> &nums, int x)
+    {
+        return solve(nums, 0, nums.size() - 1, x);
+    }
 };
