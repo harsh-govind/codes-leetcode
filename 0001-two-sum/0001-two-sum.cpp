2d6368c647cfcd3f6b2b335cf4e77f550c8f23e6
Harsh Govind <harsh5092001@gmail.com>
2023-11-12 14:26:10 +0530

    Update 0001-two-sum.cpp

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,23 +1,23 @@
 class Solution
 {
-    public:
-        vector<int> twoSum(vector<int> &v, int key)
+public:
+    vector<int> twoSum(vector<int> &v, int key)
+    {
+        // test
+        unordered_map<int, int> s;
+        int n = v.size();
+        vector<int> ans;
+        for (int i = 0; i < n; i++)
         {
-            //test
-            unordered_map<int, int> s;
-            int n = v.size();
-            vector<int> ans;
-            for (int i = 0; i < n; i++)
+            if (s.find(key - v[i]) != s.end())
             {
-                if (s.find(key - v[i]) != s.end())
-                {
-                    //temp
-                    ans.push_back(i);
-                    ans.push_back(s[key - v[i]]);
-                    return ans;
-                }
-                s[v[i]] = i;
+                // temp
+                ans.push_back(i);
+                ans.push_back(s[key - v[i]]);
+                return ans;
             }
-            return ans;
+            s[v[i]] = i;
         }
+        return ans;
+    }
 };
